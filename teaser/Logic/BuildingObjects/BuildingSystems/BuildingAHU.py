b61df282587dd53abfa5aff5d11a853b421c580c
PRemmen <Premmen@eonerc.rwth-aachen.de>
2015-11-24 14:19:13 +0100

    Add additional parameter to AHU, Refine Export, #50

--- teaser/Logic/BuildingObjects/BuildingSystems/BuildingAHU.py
+++ teaser/Logic/BuildingObjects/BuildingSystems/BuildingAHU.py
@@ -21,7 +21,29 @@ class BuildingAHU(object):
 
     Attributes
     ----------
-
+    heating : Boolean (default = True)
+        Heating Function of AHU
+    cooling : Boolean (default = True)
+        Cooling Function of AHU
+    dehumidification : Boolean (default = True)
+        Dehumidification Function of AHU (Cooling and Heating must be enabled)
+    humidification : Boolean (default = True)
+        Humidification Function of AHU (Cooling and Heating must be enabled)
+    heat_recovery : Boolean (default = True)
+        Is a HeatRecoverySystem physically integrated in the AHU? in AixLib:
+        "HRS"
+    by_pass_dehumidification : float (default = 0.2)
+         By-pass factor of cooling coil during dehumidification. Necessary to 
+         calculate the real outgoing enthalpy flow of heat exchanger in 
+         dehumidification mode taking the surface enthalpy of the cooling 
+         coil into account. In AixLib called "BPF_DeHu"
+    efficiency_recovery : float (default = 0.8)
+        efficiency of HRS in the AHU modes when HRS is enabled. in AixLib:
+        "efficiencyHRS_enabled"
+    efficiency_revocery_false : float (default = 0.2)
+        taking a little heat transfer into account although HRS is disabled 
+        (in case that a HRS is physically installed in the AHU) in AixLib:
+        "efficiencyHRS_disabled"
     profile_relative_humidity : [float]
         timeline of relative humidity requirements for AHU simulation
     profile_status_AHU : [Boolean]
@@ -35,6 +57,16 @@ class BuildingAHU(object):
         '''Constructor of BuildingAHU Class
         '''
         self.parent = parent
+        
+        self.heating = True
+        self.cooling = True
+        self.dehumidification = True
+        self.humidification = True
+        self.heat_recovery = True
+        self.by_pass_dehumidification = 0.2
+        self.efficiency_recovery = 0.8
+        self.efficiency_revocery_false = 0.2
+
         self.profile_min_relative_humidity = []
         self.profile_max_relative_humidity = []
         self.profile_status_AHU = []
