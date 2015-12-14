ddf2661715ad255370cdaf07a5fdf0e6878f5502
mla-mmn <mmans@eonerc.rwth-aachen.de>
2015-12-14 10:53:37 +0100

    #13 integrated initial temperature and Imax

--- teaser/Logic/Simulation/ModelicaInfo.py
+++ teaser/Logic/Simulation/ModelicaInfo.py
@@ -1,17 +1,18 @@
 # created November 2015
 # by TEASER4 Development Team
 
-class ModelicaInfo():
 
+class ModelicaInfo():
 
     def __init__(self):
         '''Constructor of Simulation Class.
 
         '''
-        
+
         # Base-Values for the simulation tab
         self.runtime_simulation = "31536000"
         self.interval_output = "3600"
         self.solver = ["Lsodar", "dassl"]
         self.current_solver = "dassl"
         self.equidistant_output = True
+        self.initial_temperature = 273.15 + 20.1
