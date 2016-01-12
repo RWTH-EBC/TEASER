50a824a072ec5f926912d91c61ec6d8a8b70100e
PRemmen <Premmen@eonerc.rwth-aachen.de>
2016-01-12 14:57:03 +0100

    default values for building elements, #71

--- teaser/Logic/BuildingObjects/BuildingPhysics/Floor.py
+++ teaser/Logic/BuildingObjects/BuildingPhysics/Floor.py
@@ -13,3 +13,8 @@ class Floor(InnerWall):
         '''
         '''
         super(Floor, self).__init__(parent)
+        
+        self._tilt = 90.0
+        self._orientation = -2.0
+        self._inner_convection = 1.7
+        self._inner_radiation = 5.0
