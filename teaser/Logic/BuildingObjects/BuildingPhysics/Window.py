88ddeef91dd49eaf9d6226199cf85fd0c49a5c0f
PRemmen <Premmen@eonerc.rwth-aachen.de>
2016-01-13 19:04:41 +0100

    Take Window.py from branch #99 to get additional attributes, #51

--- teaser/Logic/BuildingObjects/BuildingPhysics/Window.py
+++ teaser/Logic/BuildingObjects/BuildingPhysics/Window.py
@@ -32,13 +32,13 @@ class Window(BuildingElement):
         self._a_conv = 0.0
         self._shading_g_total = 0.0
         self._shading_max_irr = 0.0
-        
+        self.splitfac = 0.0  # splitfac
+        self.imax = 200  # Intensity where sunblind closes
         self._tilt = 90.0
         self._inner_convection = 2.7
         self._inner_radiation = 5.0
         self._outer_convection = 20.0
         self._outer_radiation = 5.0
-
     def calc_equivalent_res(self):
         '''Equivalent resistance VDI 6007
 
@@ -52,7 +52,7 @@ class Window(BuildingElement):
         '''
         self.set_calc_default()
         number_of_layer, density, thermal_conduc, heat_capac, thickness = \
-        self.gather_element_properties()
+            self.gather_element_properties()
 
         r_layer = thickness/thermal_conduc
         c_layer = heat_capac*density*thickness  # *1000
