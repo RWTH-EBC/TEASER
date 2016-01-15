f89e8a3dacc4867910be9fce7e78e910a2b6ed3e
PRemmen <Premmen@eonerc.rwth-aachen.de>
2016-01-15 16:10:54 +0100

    GUI for development

---
+++ teaser/GUI/GUIHelperClasses/TrackableItem.py
@@ -0,0 +1,12 @@
+# created June 2015
+# by TEASER4 Development Team
+
+from PyQt4 import QtGui
+
+class TrackableItem (QtGui.QStandardItem):
+    def __init__(self, text, internal_id):
+        QtGui.QStandardItem.__init__(self, text)
+        
+        self.internal_id = internal_id
+        self.setText(text)
+
