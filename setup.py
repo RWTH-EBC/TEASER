924860a84abfde9244e0d430f71ac6e7b89f3b10
PRemmen <Premmen@eonerc.rwth-aachen.de>
2016-01-29 14:17:34 +0100

    Change Example and Version number, #125

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='teaser',
-      version='0.2.1',
+      version='0.2.3',
       description='Tool for Energy Analysis and Simulation for Efficient Retrofit ',
       url='https://github.com/RWTH-EBC/TEASER',
       author='TEASER Development Team',
