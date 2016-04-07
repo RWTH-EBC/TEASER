0d2f162d635939665986eaf455b7a9ca019cb26a
PRemmen <PRemmen@eonerc.rwth-aachen.de>
2016-04-07 17:37:55 +0200

    Surfacegml

--- teaser/Data/Input/citygml_input.py
+++ teaser/Data/Input/citygml_input.py
@@ -38,53 +38,162 @@ def load_gml(path, prj):
     gml_bind = citygml.CreateFromDocument(xml_file.read())
 
     for i, city_object in enumerate(gml_bind.featureMember):
-        #for LOD2 we check if boundary surfaces are described
+        #for LOD2 is used we check if boundary surfaces are described
         if city_object.Feature.boundedBy_:
             bldg = Building(parent=prj)
             for bound_surf in city_object.Feature.boundedBy_:
-                for comp_member in  bound_surf.BoundarySurface.lod2MultiSurface.MultiSurface.surfaceMember:
+                for comp_member in bound_surf.BoundarySurface.lod2MultiSurface.MultiSurface.surfaceMember:
                     try: #modelling option 1
-                        _identify_area(bldg,comp_member.Surface.exterior.Ring.posList.value())
+                        Surface_gml(
+                            comp_member.Surface.exterior.Ring.posList.value())
                     except: #modelling option 2
                         for pos_list in comp_member.Surface.surfaceMember:
-                            _identify_area(bldg, pos_list.Surface.exterior.Ring.posList.value())
+                            Surface_gml(pos_list.Surface.exterior.Ring.posList.value())
+
+class Surface_gml(object):
+
+    def __init__(self, gml_surface, boundary = None):
+        self.gml_surface = gml_surface
+        self.surface_area = None
+        self.surface_orientation = None
+        self.surface_tilt = None
+        self.name = boundary
+
+        self.surface_area = self.get_gml_area()
+        self.surface_orientation = self.get_gml_orientation()
+        self.surface_tilt = self.get_gml_tilt()
+
+    def get_gml_area(self):
+        '''calc the area of a gml_surface defined by 4 or 5 gml coordinates
+
+        Surface needs to be planar
+
+        Parameters
+        ----------
+        gml_surface : [float]
+            list of gml coordinates that describe a planar surface
+
+        Returns
+        ----------
+        surface_area : float
+            returns the area of the surface
+        '''
+        gml_surface = np.array(self.gml_surface)
+
+        gml1 = gml_surface[0:3]
+        gml2 = gml_surface[3:6]
+        gml3 = gml_surface[6:9]
+        gml4 = gml_surface[9:12]
+
+        vektor_1 = gml2-gml1
+        vektor_2 = gml3-gml1
+        normal_1 = np.cross(vektor_1, vektor_2)
+
+        self.surface_area = LA.norm(normal_1)
+
+        if len(gml_surface) > 15:
+            vektor_3 = gml4-gml3
+            normal_2 = np.cross(vektor_1, vektor_3)
+            self.surface_area = 0.5*LA.norm(normal_2) + self.surface_area
+
+        return self.surface_area
+
+    def get_gml_tilt(self):
+        '''calc the tilt of a gml_surface defined by 4 or 5 gml coordinates
+
+        Surface needs to be planar
+
+        Parameters
+        ----------
+        gml_surface : [float]
+            list of gml coordinates that describe a planar surface
+
+        Returns
+        ----------
+        surface_tilt : float
+            returns the orientation of the surface
+        '''
+
+        gml_surface = np.array(self.gml_surface)
+        gml1 = gml_surface[0:3]
+        gml2 = gml_surface[3:6]
+        gml3 = gml_surface[6:9]
+        gml4 = gml_surface[9:12]
+
+        vektor_1 = gml2-gml1
+        vektor_2 = gml3-gml1
+
+        normal_1 = np.cross(vektor_1, vektor_2)
+        z_axis = np.array([0,0,1])
+
+        self.surface_tilt = np.arccos(np.dot(normal_1, z_axis)/(LA.norm(
+            z_axis)*LA.norm(normal_1)))*360/(2*np.pi)
+
+        if self.surface_tilt == 180:
+            self.surface_tilt = 0.0
+
+        return self.surface_tilt
 
 
+    def get_gml_orientation(self):
+        '''calc the orienation of a gml_surface defined by 4 or 5 gml
+        coordinates
 
-def _identify_area(teaser_bldg, pos_list):
+        Surface needs to be planar, the orientation returned is in TEASER
+        coordinates
 
-    print(_get_gml_tilt(pos_list=pos_list))
+        Parameters
+        ----------
+        gml_surface : [float]
+            list of gml coordinates that describe a planar surface
+
+        Returns
+        ----------
+        surface_orientation : float
+            returns the orientation of the surface
+        '''
+
+        gml_surface = np.array(self.gml_surface)
+        gml1 = gml_surface[0:3]
+        gml2 = gml_surface[3:6]
+        gml3 = gml_surface[6:9]
+        gml4 = gml_surface[9:12]
+
+        vektor_1 = gml2-gml1
+        vektor_2 = gml3-gml1
+
+        normal_1 = np.cross(vektor_1, vektor_2)
+        normal_uni = normal_1/LA.norm(normal_1)
+        phi = None
+        if normal_uni[0] > 0:
+            phi = np.arctan(normal_uni[1]/normal_uni[0])
+        elif normal_uni[0] < 0 <= normal_uni[1]:
+            phi = np.arctan(normal_uni[1]/normal_uni[0]) + np.pi
+        elif normal_uni[0] < 0 > normal_uni[1]:
+            phi = np.arctan(normal_uni[1]/normal_uni[0]) - np.pi
+        elif normal_uni[0] == 0 < normal_uni[1]:
+            phi = np.pi/2
+        elif normal_uni[0] == 0 > normal_uni[1]:
+            phi = -np.pi/2
 
 
-def _get_gml_tilt(pos_list):
-    '''calc the tilt of a gml_surface defined by 4 or 5 gml coordinates
+        if phi is None:
+            pass
+        elif phi < 0:
+            self.surface_orientation = (phi+2*np.pi)*360/(2*np.pi)
+        else:
+            self.surface_orientation = (phi)*360/(2*np.pi)
 
-    Surface needs to be planar
+        if self.surface_orientation is None:
+            pass
+        elif 0 <= self.surface_orientation <= 90:
+            self.surface_orientation = 90 - self.surface_orientation
+        else:
+            self.surface_orientation = 450 - self.surface_orientation
 
-    Parameters
-    ----------
-    pos_list : [float]
-        list of gml coordinates that describe a planar surface
+        if normal_uni[2] == -1:
+            self.surface_orientation = -2
+        elif normal_uni[2] == 1:
+            self.surface_orientation = -1
 
-    Returns
-    ----------
-    surface_tilt : float
-        returns the orientation of the surface
-    '''
-
-    gml_surface = np.array(pos_list)
-    gml1 = gml_surface[0:3]
-    gml2 = gml_surface[3:6]
-    gml3 = gml_surface[6:9]
-    vector_1 = gml2-gml1
-    vector_2 = gml3-gml1
-
-    normal_1 = np.cross(vector_1, vector_2)
-    z_axis = np.array([0,0,1])
-
-    surface_tilt = np.arccos(np.dot(normal_1, z_axis)/(LA.norm(z_axis)*LA.norm(normal_1)))*360/(2*np.pi)
-
-    if surface_tilt == 180:
-        surface_tilt = 0.0
-
-    return surface_tilt
+        return self.surface_orientation
