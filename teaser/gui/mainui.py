cd9523b8a93dae120e7a1262161a928985e8e0de
yGoerguelue <yasin.goerguelue@rwth-aachen.de>
2016-09-19 14:37:15 +0200

    #286 delete button works, but there is a little bug that only allows one wall to be deleted

--- teaser/gui/mainui.py
+++ teaser/gui/mainui.py
@@ -1713,7 +1713,8 @@ class MainUI(QDialog):
 
         self.xml_ui_del_button = QtGui.QPushButton()
         self.xml_ui_del_button.setText("Delete")
-        # self.connect(self.xml_ui_del_button, SIGNAL("clicked()"),
+        self.connect(self.xml_ui_del_button, SIGNAL("clicked()"),
+                     self.delete_wall_in_xml)
         self.xml_ui_del_button.setMinimumWidth(340)
 
         self.xml_ui_wall_list_view = QtGui.QListView()
@@ -3943,6 +3944,72 @@ class MainUI(QDialog):
         ax_p.set_ylim([0, 1])
         self.canvas_profiles.draw()
 
+    def update_wall_details(self):
+        '''Update the wall details window
+
+        updates the wall details window after wall have been changed.
+        '''
+
+        self.element_model_update_xml.clear()
+        for inner_wall in self.thermalZoneFromXML.inner_walls:
+            if type(inner_wall).__name__ == "InnerWall":
+                item = TrackableItem(
+                    "Type:\t".expandtabs(8) + inner_wall.name +
+                    "\nconstruction_type:\t".expandtabs(11) +
+                    str(inner_wall.construction_type) +
+                    "\nbuilding_age_group:\t".expandtabs(11) +
+                    str(inner_wall.building_age_group), inner_wall.internal_id)
+                self.element_model_update_xml.appendRow(item)
+            if type(inner_wall).__name__ == "Floor":
+                item = TrackableItem(
+                    "Type:\t".expandtabs(8) + inner_wall.name +
+                    "\nconstruction_type:\t".expandtabs(11) +
+                    str(inner_wall.construction_type) +
+                    "\nbuilding_age_group:\t".expandtabs(11) +
+                    str(inner_wall.building_age_group), inner_wall.internal_id)
+                self.element_model_update_xml.appendRow(item)
+            if type(inner_wall).__name__ == "Ceiling":
+                item = TrackableItem(
+                    "Type:\t".expandtabs(8) + inner_wall.name +
+                    "\nconstruction_type:\t".expandtabs(11) +
+                    str(inner_wall.construction_type) +
+                    "\nbuilding_age_group:\t".expandtabs(11) +
+                    str(inner_wall.building_age_group), inner_wall.internal_id)
+                self.element_model_update_xml.appendRow(item)
+        for wall in self.thermalZoneFromXML.outer_walls:
+            if type(wall).__name__ == "GroundFloor":
+                item = TrackableItem(
+                    "Type:\t".expandtabs(8) + wall.name +
+                    "\nconstruction_type:\t".expandtabs(11) +
+                    str(wall.construction_type) +
+                    "\nbuilding_age_group:\t".expandtabs(11) +
+                    str(wall.building_age_group), wall.internal_id)
+                self.element_model_update_xml.appendRow(item)
+            if type(wall).__name__ == "Rooftop":
+                item = TrackableItem(
+                    "Type:\t".expandtabs(8) + wall.name +
+                    "\nconstruction_type:\t".expandtabs(11) +
+                    str(wall.construction_type) +
+                    "\nbuilding_age_group:\t".expandtabs(11) +
+                    str(wall.building_age_group), wall.internal_id)
+                self.element_model_update_xml.appendRow(item)
+            if type(wall).__name__ == "OuterWall":
+                item = TrackableItem(
+                    "Type:\t".expandtabs(8) + wall.name +
+                    "\nconstruction_type:\t".expandtabs(11) +
+                    str(wall.construction_type) +
+                    "\nbuilding_age_group:\t".expandtabs(11) +
+                    str(wall.building_age_group), wall.internal_id)
+                self.element_model_update_xml.appendRow(item)
+        for window in self.thermalZoneFromXML.windows:
+                item = TrackableItem(
+                    "Type:\t".expandtabs(8) + window.name +
+                    "\nconstruction_type:\t".expandtabs(11) +
+                    str(wall.construction_type) +
+                    "\nbuilding_age_group:\t".expandtabs(11) +
+                    str(wall.building_age_group), window.internal_id)
+                self.element_model_update_xml.appendRow(item)
+
     def update_element_details(self):
         '''Update the element details window
 
@@ -5900,6 +5967,39 @@ class MainUI(QDialog):
                             ind = zone.windows.index(element)
                             del zone.windows[ind]
                             self.update_zone_details()
+        except (ValueError, AttributeError):
+            QtGui.QMessageBox.warning(self,
+                                      u"No element selected",
+                                      u"You need to select an element first.")
+
+    def delete_wall_in_xml(self):
+        '''Deletes a wall
+
+        checks if a wall is currently selected and deletes the selected wall.
+        '''
+
+        try:
+            item = self.element_model_update_xml.itemFromIndex(
+                    self.xml_ui_wall_list_view.currentIndex())
+            zone = self.thermalZoneFromXML
+            for wall in self.thermalZoneFromXML.inner_walls:
+                if self.thermalZoneFromXML.inner_walls.index(wall) == item.internal_id:
+                    ind = zone.inner_walls.index(wall)
+                    del zone.inner_walls[ind]
+                    self.update_wall_details()
+
+            for wall in zone.outer_walls:
+                if self.thermalZoneFromXML.outer_walls.index(wall) == item.internal_id:
+                    ind = zone.outer_walls.index(wall)
+                    del zone.outer_walls[ind]
+                    self.update_wall_details()
+
+            for wall in self.thermalZoneFromXML.windows:
+                if self.thermalZoneFromXML.windows.index(wall) == item.internal_id:
+                    ind = zone.windows.index(wall)
+                    del zone.windows[ind]
+                    self.update_wall_details()
+
         except (ValueError, AttributeError):
             QtGui.QMessageBox.warning(self,
                                       u"No element selected",
