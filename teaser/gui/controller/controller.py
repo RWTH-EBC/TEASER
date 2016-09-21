94bc66e2a28fc9be6d9bf3802810ac6b80f9c023
yGoerguelue <yasin.goerguelue@rwth-aachen.de>
2016-09-21 13:48:07 +0200

    #286 deleted unnecessary source code

--- teaser/gui/controller/controller.py
+++ teaser/gui/controller/controller.py
@@ -962,12 +962,6 @@ class Controller():
         element.save_type_element(prefix_path, suffix_path)
 
     @classmethod
-    def modify_element_in_xml(self, element, path):
-        'dummy'
-        print("dummy")
-
-    @classmethod
     def delete_element_in_xml(self, element, path):
         prefix_path, suffix_path = utilitis.split_path(path)
-        print("delete")
         element.delete_type_element(prefix_path, suffix_path)
