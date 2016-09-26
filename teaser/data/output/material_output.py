e0b626fc8d3b05367e769e8660244a7c079712ab
PRemmen <PRemmen@eonerc.rwth-aachen.de>
2016-09-26 16:08:47 +0200

    add check to material

--- teaser/data/output/material_output.py
+++ teaser/data/output/material_output.py
@@ -7,7 +7,7 @@ This module contains function to save material classes
 """
 import teaser.data.bindings.v_0_4.material_bind as mat_bind
 import teaser.logic.utilities as utilitis
-
+import warnings
 
 def save_material(material, data_class):
     """Material saver.
@@ -25,17 +25,32 @@ def save_material(material, data_class):
         but the user can individually change that.
 
     """
-
-    mat_pyxb = mat_bind.MaterialType()
-
-    mat_pyxb.name = material.name
-    mat_pyxb.density = material.density
-    mat_pyxb.thermal_conduc = material.thermal_conduc
-    mat_pyxb.heat_capac = material.heat_capac
-    mat_pyxb.material_id = material.material_id
-
     mat_binding = data_class.material_bind
-    mat_binding.Material.append(mat_pyxb)
-    out_file = open(utilitis.get_full_path(data_class.path_mat), "w")
+    add_to_xml = True
+    mat_binding.version = "0.4"
+    warning_text = ("Material with same name and same properties already "
+                    "exists in XML, consider this material or revising your "
+                    "properties")
 
-    out_file.write(mat_binding.toDOM().toprettyxml())
+    for check in mat_binding.Material:
+        if check.name == material.name and \
+                check.density == material.density and \
+                check.thermal_conduc == material.thermal_conduc and \
+                check.heat_capac == material.heat_capac:
+            warnings.warn(warning_text)
+            add_to_xml = False
+            break
+
+    if add_to_xml is True:
+        mat_pyxb = mat_bind.MaterialType()
+
+        mat_pyxb.name = material.name
+        mat_pyxb.density = material.density
+        mat_pyxb.thermal_conduc = material.thermal_conduc
+        mat_pyxb.heat_capac = material.heat_capac
+        mat_pyxb.material_id = material.material_id
+
+        mat_binding.Material.append(mat_pyxb)
+        out_file = open(utilitis.get_full_path(data_class.path_mat), "w")
+
+        out_file.write(mat_binding.toDOM().toprettyxml())
