85693218f41efc2553a5efec25ed9583958fa780
PRemmen <PRemmen@eonerc.rwth-aachen.de>
2016-09-28 13:58:58 +0200

    prepare use conditions

--- teaser/data/dataclass.py
+++ teaser/data/dataclass.py
@@ -90,24 +90,37 @@ class DataClass(object):
             self.element_bind = tb_bind.CreateFromDocument(__xml_file_tb.read())
 
     def load_uc_binding(self):
-        """loads specified type building XML files into bindings
+        """loads specified use condition XML files into bindings
 
         """
 
-        version_parse = element_tree.parse(self.path_uc)
-        __xml_file_uc = open(self.path_uc, 'r')
+        try:
+            __xml_file_uc = open(self.path_uc, 'r+')
+            version_parse = element_tree.parse(self.path_tb)
+        except:
+            __xml_file_uc = open(self.path_uc, 'w')
+            version_parse = False
 
-        if bool(version_parse.getroot().attrib) is False:
+        if version_parse is False:
+            import teaser.data.bindings.v_0_4.boundaryconditions_bind as uc_bind
+            self.conditions_bind = uc_bind.UseConditions()
+        elif bool(version_parse.getroot().attrib) is False:
             warnings.warn("You are using an old version of use condition data base XML file")
             import teaser.data.bindings.v_0_3_9.boundaryconditions_bind as uc_bind
+            self.conditions_bind = uc_bind.CreateFromDocument(
+                __xml_file_uc.read())
         elif version_parse.getroot().attrib['version'] == "0.3.9":
+            warnings.warn("You are using an old version of use condition data base XML file")
             import teaser.data.bindings.v_0_3_9.boundaryconditions_bind as uc_bind
-
-        self.conditions_bind = uc_bind.CreateFromDocument(__xml_file_uc.read())
-
+            self.conditions_bind = uc_bind.CreateFromDocument(
+                __xml_file_uc.read())
+        elif version_parse.getroot().attrib['version'] == "0.4":
+            import teaser.data.bindings.v_0_4.boundaryconditions_bind as uc_bind
+            self.conditions_bind = uc_bind.CreateFromDocument(
+                __xml_file_uc.read())
 
     def load_mat_binding(self):
-        """loads specified type building XML files into bindings
+        """loads specified material XML files into bindings
 
         """
         try:
