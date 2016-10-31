ea1f1dc41402c2a5f08453970e6ded8be25e7b23
MichaMans <michael.mans@hotmail.com>
2016-10-31 15:42:34 +0100

    #335 revised the if statement to and elif statement

--- teaser/data/dataclass.py
+++ teaser/data/dataclass.py
@@ -131,7 +131,7 @@ class DataClass(object):
         if version_parse is False:
             import teaser.data.bindings.v_0_4.material_bind as mat_bind
             self.material_bind = mat_bind.MaterialTemplates()
-        if bool(version_parse.getroot().attrib) is False:
+        elif bool(version_parse.getroot().attrib) is False:
             warnings.warn(
                 "You are using an old version of material data base XML file")
             import teaser.data.bindings.v_0_3_9.material_bind as mat_bind
@@ -147,4 +147,3 @@ class DataClass(object):
             import teaser.data.bindings.v_0_4.material_bind as mat_bind
             self.material_bind = mat_bind.CreateFromDocument(
                 __xml_file_mat.read())
-
