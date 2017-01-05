03ef877a31b874c908a1fcc844614255cc61e9ff
PRemmen <PRemmen@eonerc.rwth-aachen.de>
2017-01-05 14:05:33 +0100

    example 6

--- teaser/examples/e6_generate_building.py
+++ teaser/examples/e6_generate_building.py
@@ -38,7 +38,7 @@ def example_create_building():
     bldg.name = "SuperExampleBuilding"
     bldg.street_name = "AwesomeAvenue42"
     bldg.city = "46325FantasticTown"
-    bldg.year_of_construction = 1988
+    bldg.year_of_construction = 2015
     bldg.number_of_floors = 1
     bldg.height_of_floors = 3.5
 
@@ -67,7 +67,9 @@ def example_create_building():
     # north = 0°). Setting the the ThermalZone as a parent will automatically
     # assign this element to the thermal zone. We also set names, tilt and
     # coefficients for heat transfer on the inner and outer side of the
-    # roofs. Please read the docs to get more information on these parameters.
+    # roofs. If the building has a flat roof, please use -1 as
+    # orientation. Please read the docs to get more information on these
+    # parameters.
 
     from teaser.logic.buildingobjects.buildingphysics.rooftop import Rooftop
 
@@ -101,7 +103,7 @@ def example_create_building():
     # First layer south
 
     layer_s1 = Layer(parent=roof_south, id=0)
-    layer_s1.thickness = 0.15
+    layer_s1.thickness = 0.3
 
     from teaser.logic.buildingobjects.buildingphysics.material import Material
 
@@ -125,7 +127,7 @@ def example_create_building():
     # First layer north
 
     layer_n1 = Layer(parent=roof_south, id=0)
-    layer_n1.thickness = 0.15
+    layer_n1.thickness = 0.3
 
     from teaser.logic.buildingobjects.buildingphysics.material import Material
 
@@ -146,154 +148,119 @@ def example_create_building():
     material_n2.heat_capac = 0.6
     material_n2.thermal_conduc = 2.5
 
+    # Another option is to use the database for typical wall constructions,
+    # but set area, tilt, orientation individually. To simplifiy code,
+    # we save individual information for exterior walls, interior walls into
+    # dictionaries.
+    # outer walls
+    # {'name_of_wall': [area, tilt, orientation]}
+    # interior walls
+    # {'name_of_wall': [area, tilt, orientation]}
 
+    from teaser.logic.buildingobjects.buildingphysics.outerwall import OuterWall
 
-    '''We save information of the Outer and Inner walls as well as Windows
-    in dicts, the key is the name, while the value is a list (if applicable)
-    [year of construciton,
-     construction type,
-     area,
-     tilt,
-     orientation]
-     '''
+    out_wall_dict = {"OuterWall_north": [10.0, 90.0, 0.0],
+                     "OuterWall_east": [14.0, 90.0, 90.0],
+                     "OuterWall_south": [10.0, 90.0, 180.0],
+                     "OuterWall_west": [14.0, 90.0, 270.0]}
 
-    out_wall_dict = {"Outer Wall 1": [bldg.year_of_construction, 'heavy',
-                                      10.0, 90.0, 0.0],
-                     "Outer Wall 2": [bldg.year_of_construction, 'heavy',
-                                      14.0, 90.0, 90.0],
-                     "Outer Wall 3": [bldg.year_of_construction, 'heavy',
-                                      10.0, 90.0, 180.0],
-                     "Outer Wall 4": [bldg.year_of_construction, 'heavy',
-                                      14.0, 90.0, 270.0]}
+    # For ground floors the orientation is always -2
 
-    in_wall_dict = {"Inner Wall 1": [bldg.year_of_construction, 'light', 10.0],
-                    "Inner Wall 2": [bldg.year_of_construction, 'heavy', 14.0],
-                    "Inner Wall 3": [bldg.year_of_construction, 'light', 10.0]}
+    ground_floor_dict = {"GroundFloor": [100.0, 0.0, -2]}
 
-    win_dict = {"Window 1": [bldg.year_of_construction,
-                             5.0, 90.0, 90.0],
-                "Window 2": [bldg.year_of_construction,
-                             8.0, 90.0, 180.0],
-                "Window 3": [bldg.year_of_construction,
-                             5.0, 90.0, 270.0]}
+    from teaser.logic.buildingobjects.buildingphysics.innerwall import InnerWall
+
+    in_wall_dict = {"InnerWall1": [bldg.year_of_construction, 'light', 10.0],
+                    "InnerWall2": [bldg.year_of_construction, 'heavy', 14.0],
+                    "InnerWall3": [bldg.year_of_construction, 'light', 10.0]}
 
     for key, value in out_wall_dict.items():
-        '''instantiate OuterWall class'''
-        out_wall = OuterWall(parent = tz)
+        # Instantiate class, key is the name
+        out_wall = OuterWall(parent=tz)
         out_wall.name = key
-        '''load typical construction, based on year of construction and
-        construction type'''
-        out_wall.load_type_element(year=value[0],
-                                   construction=value[1], data_class=prj.data)
-        out_wall.area = value[2]
-        out_wall.tilt = value[3]
-        out_wall.orientation = value[4]
+        # Use load_type_element() function of the building element, and pass
+        # over the year of construction of the building and the type of
+        # construction (in this case `heavy`).
+
+        out_wall.load_type_element(
+            year=bldg.year_of_construction,
+            construction='heavy')
+
+        # area, tilt and orientation need to be set individually.
+
+        out_wall.area = value[0]
+        out_wall.tilt = value[1]
+        out_wall.orientation = value[2]
+
+    # Repeat the procedure for inner walls and ground floors
 
     for key, value in in_wall_dict.items():
-        '''instantiate InnerWall class'''
-        in_wall = InnerWall(parent = tz)
+
+        in_wall = InnerWall(parent=tz)
         in_wall.name = key
-        '''load typical construction, based on year of construction and
-        construction type'''
-        in_wall.load_type_element(year=value[0],
-                                  construction=value[1], data_class=prj.data)
-        in_wall.area = value[2]
+        in_wall.load_type_element(
+            year=bldg.year_of_construction,
+            construction='heavy')
+        in_wall.area = value[0]
+
+    from teaser.logic.buildingobjects.buildingphysics.groundfloor import \
+        GroundFloor
+
+    for key, value in ground_floor_dict.items():
+
+        ground = GroundFloor(parent=tz)
+        ground.name = key
+        ground.load_type_element(
+            year=bldg.year_of_construction,
+            construction='heavy')
+        ground.area = value[0]
+        ground.tilt = value[1]
+        ground.orientation = value[1]
+
+    from teaser.logic.buildingobjects.buildingphysics.window import Window
+
+    win_dict = {"Window_east": [5.0, 90.0, 90.0],
+                "Window_south": [8.0, 90.0, 180.0],
+                "Window_west": [5.0, 90.0, 270.0]}
 
     for key, value in win_dict.items():
-        '''instantiate Window class'''
+
         win = Window(parent = tz)
         win.name = key
         win.area = value[1]
         win.tilt = value[2]
         win.orientation = value[3]
 
-        '''
-        We know the exact properties of the window, thus we set them instead
-        of loading a typical construction
-        '''
+        # Additional to the already known attributes the window has
+        # additional attributes. Window.g_value describes the solar gain
+        # through windows, a_conv the convective heat transmission due to
+        # absorption of the window on the inner side. shading_g_total and
+        # shading_max_irr refers to the shading (solar gain reduction of the
+        # shading and shading_max_irr the threshold of irradiance to
+        # automatically apply shading).
+
         win.inner_convection = 1.7
         win.inner_radiation = 5.0
         win.outer_convection = 20.0
         win.outer_radiation = 5.0
         win.g_value = 0.789
         win.a_conv = 0.03
-        win.shading_g_total = 1.0
+        win.shading_g_total = 0.0
         win.shading_max_irr = 180.0
-        '''Instantiate a Layer class, with window as parent, set attributes'''
-        win_layer = Layer(parent = win)
+
+        # One equivalent layer for windows
+
+        win_layer = Layer(parent=win)
         win_layer.id = 1
         win_layer.thickness = 0.024
-        '''Instantiate a Material class, with window layer as parent,
-        set attributes'''
+
+        # Material for Glas
+
         win_material = Material(win_layer)
         win_material.name = "GlasWindow"
         win_material.thermal_conduc = 0.067
         win_material.transmittance = 0.9
 
-
-    '''For a GroundFloor we are using the load_type_element function,
-    which needs the year of construction and the construction type ('heavy'
-    or 'light')
-    '''
-    ground = GroundFloor(parent=tz)
-    ground.name = "Ground floor"
-    ground.load_type_element(bldg.year_of_construction, 'heavy', prj.data)
-    ground.area = 140.0
-
-    '''
-    We need to set the projects calculation method. The library we want to
-    use is AixLib, we are using a two element model and want an extra resistance
-    for the windows. To export the parameters to a Modelica record, we use
-    the export_aixlib function. path = None indicates, that we want to store
-    the records in TEASER'S Output folder
-    '''
-
-    prj.used_library_calc = 'AixLib'
-    prj.number_of_elements_calc = 2
-    prj.merge_windows_calc = False
-
-    prj.calc_all_buildings()
-
-    '''
-    Export the Modelica Record. If you have a Dymola License you can  export
-    the model with a central AHU (MultizoneEquipped) (only default for office
-    and institute buildings)
-    '''
-    
-    #prj.export_aixlib(building_model="MultizoneEquipped",
-    #                  zone_model="ThermalZoneEquipped",
-    #                  corG=True,
-    #                  internal_id=None,
-    #                  path=None)
-
-    '''
-    For OpenModelica you need to exclude the centralAHU (because it is using
-    state machines). Therefore use the building_model "Multizone"
-    '''
-
-    #prj.export_aixlib(building_model="Multizone",
-    #                  zone_model="ThermalZoneEquipped",
-    #                  corG=True,
-    #                  internal_id=None,
-    #                  path=None)
-
-
-    '''Or we use Annex60 method (e.g with four elements). Which exports one
-    Model per zone'''
-
-    # prj.used_library_calc = 'Annex60'
-    # prj.number_of_elements_calc = 4
-    # prj.merge_windows_calc = False
-    #
-    # prj.calc_all_buildings()
-    # prj.export_annex()
-
-    '''
-    Save new TEASER XML and cityGML
-    '''
-    prj.save_project("ExampleProject")
-    prj.save_citygml("ExampleCityGML")
-
 if __name__ == '__main__':
     example_create_building()
-    print("That's it :)")
+    print("Example 6: That's it :)")
