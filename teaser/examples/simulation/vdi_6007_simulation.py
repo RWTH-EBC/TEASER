cca4b3bcd56f55a3341fe543d690effd8510c406
Jan Schiefelbein <jschiefelbein@eonerc.rwth-aachen.de>
2017-02-07 11:23:38 +0100

    Add further TEASER parameter calls to vdi_6007_simulation.py #297

--- teaser/examples/simulation/vdi_6007_simulation.py
+++ teaser/examples/simulation/vdi_6007_simulation.py
@@ -84,27 +84,27 @@ def vdi_example_6007(thermal_zone, weather):
                  "R1o": thermal_zone.model_attr.r1_ow,
                  "C1o": thermal_zone.model_attr.c1_ow,
                  "Ao": [thermal_zone.model_attr.area_ow],
-                 "Aw": list_window_areas,
-                 "At": list_window_areas,
+                 "Aw": thermal_zone.model_attr.window_areas,
+                 "At": thermal_zone.model_attr.transparent_areas,
                  "Vair": thermal_zone.volume,
                  "rhoair": thermal_zone.density_air,
                  "cair": thermal_zone.heat_capac_air,
-                 "splitfac": thermal_zone.windows[0].a_conv,
+                 "splitfac": thermal_zone.model_attr.ratio_conv_rad_inner_win,
                  "g": thermal_zone.model_attr.weighted_g_value,
                  "alphaiwi": thermal_zone.model_attr.alpha_comb_inner_iw,
                  "alphaowi": thermal_zone.model_attr.alpha_comb_inner_ow,
                  "alphaWall": thermal_zone.model_attr.alpha_comb_outer_ow * thermal_zone.model_attr.area_ow,
-                 "alphaowo": 25.0,
+                 #"alphaowo": 25.0,
                  # TODO: Substitute with TEASER call (misc or outer walls)
                  "withInnerwalls": withInnerwalls,
-                 "aowo": 0.9,
-                 "epso": 0.1,
-                 "orientationswallshorizontal": [90, 90, 90, 90, 0],
-                 "temperatureground": 283.15,
+                 "aowo": thermal_zone.model_attr.solar_absorp_ow,
+                 #"epso": thermal_zone.model_attr.ir_emissivity_inner_ow,
+                 #"orientationswallshorizontal": [90, 90, 90, 90, 0],
+                 "temperatureground": thermal_zone.t_ground,
                  "weightfactorswall": thermal_zone.model_attr.weightfactor_ow,
                  "weightfactorswindow": thermal_zone.model_attr.weightfactor_win,
                  "weightfactorground": thermal_zone.model_attr.weightfactor_ground,
-                 "gsunblind": list_sunblind,
+                 "gsunblind": thermal_zone.model_attr.g_sunblind,
                  "Imax": i_max}
 
     #  Solar radiation input on each external area in W/m2
@@ -136,7 +136,7 @@ def vdi_example_6007(thermal_zone, weather):
                      # weight factors of the windows
                      "wfGro": thermal_zone.model_attr.weightfactor_ground,
                      # weight factor of the ground (0 if not considered)
-                     "T_Gro": 273.15 + 10,
+                     "T_Gro": thermal_zone.t_ground,
                      "alpha_wall_out": thermal_zone.model_attr.alpha_conv_outer_ow,
                      "alpha_rad_wall": thermal_zone.model_attr.alpha_rad_outer_ow,
                      "withLongwave": False}
@@ -156,9 +156,9 @@ def vdi_example_6007(thermal_zone, weather):
 
     #  Ventilation rate: Fresh air at temperature weatherTemperature in m3/s
     #  #-------------------------------------------------------
-    #  TODO: Substitute with TEASER call (or VDI call)
     # ventRate = np.zeros(timesteps)
-    ventRate = np.zeros(timesteps) + (thermal_zone.volume * 0.5 / 3600)
+    ventRate = np.zeros(timesteps) + (thermal_zone.volume *
+                                      thermal_zone.infiltration_rate / 3600)
 
     #  Internal convective gains in W
     #  #-------------------------------------------------------
@@ -168,7 +168,8 @@ def vdi_example_6007(thermal_zone, weather):
     Q_ig = np.zeros(timesteps) + 200
 
     # Radiative heat transfer coef. between inner and outer walls in W/m2K
-    alphaRad = np.zeros(timesteps) + 5
+    alphaRad = np.zeros(timesteps) + \
+               thermal_zone.model_attr.alpha_rad_inner_mean
 
     # Define set points for heating
     #  #-------------------------------------------------------
