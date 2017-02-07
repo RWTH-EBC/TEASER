62eec62dd83932746feea146ed035e93c7bc11d1
PRemmen <PRemmen@eonerc.rwth-aachen.de>
2017-02-07 12:00:09 +0100

    add weather loader to DataClass

--- teaser/data/dataclass.py
+++ teaser/data/dataclass.py
@@ -7,7 +7,9 @@
 import teaser.logic.utilities as utils
 import warnings
 import xml.etree.ElementTree as et
-
+import os
+import numpy as np
+import math
 
 class DataClass(object):
     """Class for XML data bindings
@@ -15,6 +17,9 @@ class DataClass(object):
     This class loads all XML files with statistic or template data needed
     for statistical data enrichment. It creates the binding classes
     automatically with instantiation. The binding needs the Python Package PyXB.
+
+    If VDI Simulation in Python is used, this class loads and stores weather
+    data from TRY in a dictionary.
 
     Attributes
     ----------
@@ -37,6 +42,15 @@ class DataClass(object):
         Full path to UseConditions XML file (XML needs to be compliant
         with XSD schema). Default is
         teaser/data/input/inputdata/UseConditions.xml
+
+    weather_data : dict
+        Dictionary containing relevant weather data from Test Reference Year
+        (TRY) in following form:
+        'air_temp' : np.array - Air temperature
+        'sun_dir' : np.array - direct horizontal solar radiation
+        'sun_diff' : np.array - diffuse horizontal solar radiation
+        'rad_sky' : np.array - longwave sky radiation
+        'rad_earth' : np.array - longwave earth radiation
     """
 
     def __init__(self):
@@ -56,6 +70,13 @@ class DataClass(object):
         self.load_tb_binding()
         self.load_uc_binding()
         self.load_mat_binding()
+
+        self.weather_data = {
+            'air_temp': None,
+            'sun_dir': None,
+            'sun_diff': None,
+            'rad_sky': None,
+            'rad_earth': None}
 
     def load_tb_binding(self):
         """Loads TypeBuildingElement XML into binding classes
@@ -147,3 +168,25 @@ class DataClass(object):
             import teaser.data.bindings.v_0_4.material_bind as mat_bind
             self.material_bind = mat_bind.CreateFromDocument(
                 __xml_file_mat.read())
+
+    def load_weather(self, path):
+        """Loads Weather time series from TRY .dat file into dict
+
+        Parameters
+        ----------
+
+        path : str
+            Full path to TRY .dat file
+
+        """
+
+        wea_data = np.genfromtxt(
+            path,
+            skip_header=38,
+            usecols=(8, 13, 14, 16, 17))
+
+        self.weather_data['air_temp'] = wea_data[:, 0]
+        self.weather_data['sun_dir'] = wea_data[:, 1]
+        self.weather_data['sun_diff'] = wea_data[:, 2]
+        self.weather_data['rad_sky'] = wea_data[:, 3]
+        self.weather_data['rad_earth'] = wea_data[:, 4]
