3cfc6772dc8aa90624dcd723e10e79a61e885364
MichaMans <michael.mans@hotmail.com>
2017-02-07 16:18:16 +0100

    #297 further work on Simulation Class

--- teaser/examples/simulation/simulation_vdi_6007.py
+++ teaser/examples/simulation/simulation_vdi_6007.py
@@ -10,6 +10,9 @@ class SimulationVDI6007(object):
     Parameters
     ----------
 
+    thermal_zone : instance of ThermalZone()
+        This parameter contains the ThermalZone object related to the
+        simulation
 
     Attributes
     ----------
@@ -26,11 +29,42 @@ class SimulationVDI6007(object):
         This array contains the heat load of after the simulation with
         positive (Heating) and negative (Cooling) values
 
+    heater_limit : list
+        The heater limit list sets the maximum heat flow value for the heater
+        ToDo: we should think of standard values calculated by the normative
+        heat load
+
+    cooler_limit : list
+        The cooler limit list sets the maximum heat flow value for the cooler
 
 
         """
-    def __init__(self, arg):
+    def __init__(self, thermal_zone):
+
+        self.thermal_zone = thermal_zone
 
         self.weather = None
         self.indoor_air_temperature = None
         self.q_flow_heater_cooler = None
+
+        self.heater_limit = [1e10, 1e10, 1e10]
+        self.cooler_limit = [-1e10, -1e10, -1e10]
+
+        self.initial_air_temp
+        self.initial_inner_wall_temp
+        self.initial_outer_wall_temp
+
+    def calc_reduced_order_model(
+            weather_temperature,
+            solar_rad_in,
+            equal_air_temp,
+            alpha_rad,
+            q_internal_gains_conv,
+            q_internal_gains_rad,
+            convective_radiative_split_factor,
+            t_set_heating,
+            t_set_cooling,
+            heater_order,
+            cooler_order):
+
+        pass
