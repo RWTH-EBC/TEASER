831fc962c568956d4ef909dbd4452ee00345f113
Marcus Fuchs <m.fuchs@heatbeat.de>
2019-04-24 15:24:34 +0200

    Add option to prepare small and large thermal zone
    For #297

--- teaser/examples/verification/vdi6007_testcases/vdi6007shared.py
+++ teaser/examples/verification/vdi6007_testcases/vdi6007shared.py
@@ -13,13 +13,15 @@ from teaser.logic.buildingobjects.calculation.two_element import TwoElement
 from teaser.data.weatherdata import WeatherData
 
 
-def prepare_thermal_zone(timesteps):
+def prepare_thermal_zone(timesteps, room):
     """Prepare the thermal zone for running VDI test case
 
     Parameters
     ----------
     timesteps : int
         Number of time steps
+    room : str
+        Type of room {"S", "L"}
 
     Returns
     -------
@@ -38,20 +40,32 @@ def prepare_thermal_zone(timesteps):
 
     model_data = TwoElement(tz, merge_windows=False, t_bt=5)
 
-    # Building parameters
-    model_data.r1_iw = 0.000595693407511
-    model_data.c1_iw = 14836354.6282
-    model_data.area_iw = 75.5
-    model_data.r_rest_ow = 0.03895919557
-    model_data.r1_ow = 0.00436791293674
-    model_data.c1_ow = 1600848.94
+    if room == "S":
+        model_data.r1_iw = 0.000595693407511
+        model_data.c1_iw = 14836354.6282
+        model_data.area_iw = 75.5
+        model_data.r_rest_ow = 0.03895919557
+        model_data.r1_ow = 0.00436791293674
+        model_data.c1_ow = 1600848.94
+    elif room == "L":
+        model_data.r1_iw = 0.003237138
+        model_data.c1_iw = 7297100
+        model_data.area_iw = 75.5
+        model_data.r_rest_ow = 0.039330865
+        model_data.r1_ow = 0.00404935160802
+        model_data.c1_ow = 47900
+    else:
+        raise LookupError("Unknown room type selected. Choose from {'S', 'L'}")
+
     model_data.area_ow = 10.5
     model_data.outer_wall_areas = [10.5]
     model_data.window_areas = np.zeros(1)
     model_data.transparent_areas = np.zeros(1)
+
     tz.volume = 52.5
     tz.density_air = 1.19
     tz.heat_capac_air = 0
+
     model_data.ratio_conv_rad_inner_win = 0.09
     model_data.weighted_g_value = 1
     model_data.alpha_comb_inner_iw = 2.24
@@ -60,6 +74,8 @@ def prepare_thermal_zone(timesteps):
     model_data.alpha_rad_outer_ow = 5
     model_data.alpha_comb_outer_ow = 25
     model_data.alpha_rad_inner_mean = 5
+
+
 
     tz.model_attr = model_data
 
