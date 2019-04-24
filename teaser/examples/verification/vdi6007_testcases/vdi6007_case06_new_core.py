26b8001a35fd475b7ff638394f9af7b812f10da7
Marcus Fuchs <m.fuchs@heatbeat.de>
2019-04-24 15:38:59 +0200

    Use shared preparation for case 6
    For #297

--- teaser/examples/verification/vdi6007_testcases/vdi6007_case06_new_core.py
+++ teaser/examples/verification/vdi6007_testcases/vdi6007_case06_new_core.py
@@ -7,16 +7,10 @@
 import os
 import numpy as np
 
-from teaser.project import Project
-from teaser.logic.buildingobjects.building import Building
-from teaser.logic.buildingobjects.thermalzone import ThermalZone
-from teaser.logic.buildingobjects.calculation.two_element import TwoElement
 from teaser.logic.simulation.vdi_core import VDICore
-
-# import customized weather class
-from teaser.data.weatherdata import WeatherData
-
 import teaser.examples.verification.vdi6007_testcases.vdi6007_case01 as vdic
+from teaser.examples.verification.vdi6007_testcases.vdi6007shared import \
+    prepare_thermal_zone
 
 
 def run_case6(plot_res=False):
@@ -40,42 +34,7 @@ def run_case6(plot_res=False):
     timesteps = 24 * 60 * times_per_hour  # 60 days
     timesteps_day = int(24 * times_per_hour)
 
-    weather = WeatherData()
-    weather.air_temp = np.zeros(timesteps) + 295.15
-
-    prj = Project()
-    prj.weather_data = weather
-
-    bldg = Building(prj)
-
-    tz = ThermalZone(bldg)
-
-    model_data = TwoElement(tz, merge_windows=False, t_bt=5)
-
-    #  Store building parameters for testcase 1
-    model_data.r1_iw = 0.000595693407511
-    model_data.c1_iw = 14836354.6282
-    model_data.area_iw = 75.5
-    model_data.r_rest_ow = 0.03895919557
-    model_data.r1_ow = 0.00436791293674
-    model_data.c1_ow = 1600848.94
-    model_data.area_ow = 10.5
-    model_data.outer_wall_areas = [10.5]
-    model_data.window_areas = np.zeros(1)
-    model_data.transparent_areas = [0]
-    tz.volume = 52.5
-    tz.density_air = 1.19
-    tz.heat_capac_air = 0
-    model_data.ratio_conv_rad_inner_win = 0.09
-    model_data.weighted_g_value = 1
-    model_data.alpha_comb_inner_iw = 2.24
-    model_data.alpha_comb_inner_ow = 2.7
-    model_data.alpha_conv_outer_ow = 20
-    model_data.alpha_rad_outer_ow = 5
-    model_data.alpha_comb_outer_ow = 25
-    model_data.alpha_rad_inner_mean = 5
-
-    tz.model_attr = model_data
+    tz = prepare_thermal_zone(timesteps, room="S")
 
     calc = VDICore(tz)
     calc.equal_air_temp = np.zeros(timesteps) + 295.15
