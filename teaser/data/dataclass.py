f93a5bb22d68869f55dcc3b62c3c0623032f32a4
DaJansenGit <david.jansen1@rwth-aachen.de>
2019-09-18 11:57:05 +0200

    pep8 stuff

--- teaser/data/dataclass.py
+++ teaser/data/dataclass.py
@@ -78,7 +78,8 @@ class DataClass(object):
             "data/input/inputdata/MaterialTemplates.json"
         )
         self.conditions_bind = None
-        self.path_uc = utils.get_full_path("data/input/inputdata/UseConditions.json")
+        self.path_uc = utils.get_full_path(
+            "data/input/inputdata/UseConditions.json")
 
         self.load_uc_binding()
         self.load_mat_binding()
