8f802ad98cd477217fb955240ed1309ded4146d4
Peter Remmen <PRemmen@eonerc.rwth-aachen.de>
2020-03-03 19:52:14 +0100

    infiltration

--- teaser/logic/buildingobjects/calculation/two_element.py
+++ teaser/logic/buildingobjects/calculation/two_element.py
@@ -1163,7 +1163,7 @@ class TwoElement(object):
             (
                 (ua_value_ow_temp + self.ua_value_win)
                 + self.thermal_zone.volume
-                * self.thermal_zone.use_conditions.infiltration_rate
+                * 1.0
                 * 1
                 / 3600
                 * self.thermal_zone.heat_capac_air
