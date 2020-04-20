0eda9a834f0d1862fc0244f9c86421ca962397b9
Martin Rätz <martin.raetz@rwth-aachen.de>
2020-04-20 12:32:26 +0200

    WithProfile commit

--- teaser/logic/buildingobjects/buildingsystems/buildingahu.py
+++ teaser/logic/buildingobjects/buildingsystems/buildingahu.py
@@ -73,6 +73,9 @@ class BuildingAHU(object):
         timeline of relative humidity requirements for AHU simulation
     v_flow_profile : [int]
         timeline of desired relative v_flow of the AHU simulation (0..1)
+        note: regularly this is just a dummy, since modelica only uses
+        the rel. occupancy of a zone to calculate the supply flow.
+        This can be changed with the AixLib parameter "WithProfile"
 
     """
 
