9eed6080c55cd31c0ef61cb6da865e729b435d49
Peter Remmen <PRemmen@eonerc.rwth-aachen.de>
2020-11-06 12:07:33 +0100

    project campus

--- teaser/logic/buildingobjects/buildingphysics/wall.py
+++ teaser/logic/buildingobjects/buildingphysics/wall.py
@@ -1,6 +1,5 @@
 """asd"""
-from teaser.logic.buildingobjects.buildingphysics.buildingelement \
-    import BuildingElement
+from teaser.logic.buildingobjects.buildingphysics.buildingelement import BuildingElement
 from teaser.logic.buildingobjects.buildingphysics.layer import Layer
 from teaser.logic.buildingobjects.buildingphysics.material import Material
 import numpy as np
@@ -125,38 +124,61 @@ class Wall(BuildingElement):
             Time constant according to VDI 6007 (default t_bt = 7)
         """
 
-        nr_of_layer, density, thermal_conduc, heat_capac, thickness = \
+        nr_of_layer, density, thermal_conduc, heat_capac, thickness = (
             self.gather_element_properties()
+        )
 
         omega = 2 * np.pi / (86400 * t_bt)
 
         r_layer = thickness / thermal_conduc
         c_layer = heat_capac * density * thickness * 1000
 
-        re11 = np.cosh(np.sqrt(0.5 * omega * r_layer * c_layer)) * \
-            np.cos(np.sqrt(0.5 * omega * r_layer * c_layer))
-        im11 = np.sinh(np.sqrt(0.5 * omega * r_layer * c_layer)) * \
-            np.sin(np.sqrt(0.5 * omega * r_layer * c_layer))
-        re12 = r_layer * np.sqrt(1 / (2 * omega * r_layer * c_layer)) * \
-            (np.cosh(np.sqrt(0.5 * omega * r_layer * c_layer)) *
-                np.sin(np.sqrt(0.5 * omega * r_layer * c_layer)) +
-                np.sinh(np.sqrt(0.5 * omega * r_layer * c_layer)) *
-                np.cos(np.sqrt(0.5 * omega * r_layer * c_layer)))
-        im12 = r_layer * np.sqrt(1 / (2 * omega * r_layer * c_layer)) * \
-            (np.cosh(np.sqrt(0.5 * omega * r_layer * c_layer)) *
-                np.sin(np.sqrt(0.5 * omega * r_layer * c_layer)) -
-                np.sinh(np.sqrt(0.5 * omega * r_layer * c_layer)) *
-                np.cos(np.sqrt(0.5 * omega * r_layer * c_layer)))
-        re21 = (-1 / r_layer) * (np.sqrt(0.5 * omega * r_layer * c_layer)) * \
-            (np.cosh(np.sqrt(0.5 * omega * r_layer * c_layer)) *
-                np.sin(np.sqrt(0.5 * omega * r_layer * c_layer)) -
-                np.sinh(np.sqrt(0.5 * omega * r_layer * c_layer)) *
-                np.cos(np.sqrt(0.5 * omega * r_layer * c_layer)))
-        im21 = (1 / r_layer) * (np.sqrt(0.5 * omega * r_layer * c_layer)) * \
-            (np.cosh(np.sqrt(0.5 * omega * r_layer * c_layer)) *
-                np.sin(np.sqrt(0.5 * omega * r_layer * c_layer)) +
-                np.sinh(np.sqrt(0.5 * omega * r_layer * c_layer)) *
-                np.cos(np.sqrt(0.5 * omega * r_layer * c_layer)))
+        re11 = np.cosh(np.sqrt(0.5 * omega * r_layer * c_layer)) * np.cos(
+            np.sqrt(0.5 * omega * r_layer * c_layer)
+        )
+        im11 = np.sinh(np.sqrt(0.5 * omega * r_layer * c_layer)) * np.sin(
+            np.sqrt(0.5 * omega * r_layer * c_layer)
+        )
+        re12 = (
+            r_layer
+            * np.sqrt(1 / (2 * omega * r_layer * c_layer))
+            * (
+                np.cosh(np.sqrt(0.5 * omega * r_layer * c_layer))
+                * np.sin(np.sqrt(0.5 * omega * r_layer * c_layer))
+                + np.sinh(np.sqrt(0.5 * omega * r_layer * c_layer))
+                * np.cos(np.sqrt(0.5 * omega * r_layer * c_layer))
+            )
+        )
+        im12 = (
+            r_layer
+            * np.sqrt(1 / (2 * omega * r_layer * c_layer))
+            * (
+                np.cosh(np.sqrt(0.5 * omega * r_layer * c_layer))
+                * np.sin(np.sqrt(0.5 * omega * r_layer * c_layer))
+                - np.sinh(np.sqrt(0.5 * omega * r_layer * c_layer))
+                * np.cos(np.sqrt(0.5 * omega * r_layer * c_layer))
+            )
+        )
+        re21 = (
+            (-1 / r_layer)
+            * (np.sqrt(0.5 * omega * r_layer * c_layer))
+            * (
+                np.cosh(np.sqrt(0.5 * omega * r_layer * c_layer))
+                * np.sin(np.sqrt(0.5 * omega * r_layer * c_layer))
+                - np.sinh(np.sqrt(0.5 * omega * r_layer * c_layer))
+                * np.cos(np.sqrt(0.5 * omega * r_layer * c_layer))
+            )
+        )
+        im21 = (
+            (1 / r_layer)
+            * (np.sqrt(0.5 * omega * r_layer * c_layer))
+            * (
+                np.cosh(np.sqrt(0.5 * omega * r_layer * c_layer))
+                * np.sin(np.sqrt(0.5 * omega * r_layer * c_layer))
+                + np.sinh(np.sqrt(0.5 * omega * r_layer * c_layer))
+                * np.cos(np.sqrt(0.5 * omega * r_layer * c_layer))
+            )
+        )
         re22 = re11
         im22 = im11
 
@@ -188,47 +210,53 @@ class Wall(BuildingElement):
             new_mat = np.dot(new_mat, count_layer)
 
         # calculation of equivalent Resistance and capacities of each element
-        self.r1 = (1 / self.area) * ((new_mat[3][3] - 1) *
-                                     new_mat[0][2] + new_mat[2][3] *
-                                     new_mat[0][3]) / \
-                  ((new_mat[3][3] - 1) ** 2 + new_mat[2][3] ** 2)
-        self.r2 = (1 / self.area) * ((new_mat[0][0] - 1) *
-                                     new_mat[0][2] + new_mat[0][1] *
-                                     new_mat[0][3]) / \
-                  ((new_mat[0][0] - 1) ** 2 + new_mat[0][1] ** 2)
-        self.c1 = self.area * ((new_mat[3][3] - 1) ** 2 +
-                               (new_mat[2][3]) ** 2) / \
-            (omega * (new_mat[0][2] * new_mat[2][3] -
-                      (new_mat[3][3] - 1) * new_mat[0][3]))
-        self.c2 = self.area * ((new_mat[0][0] - 1) ** 2 +
-                               (new_mat[0][1]) ** 2) / (
-            omega * (new_mat[0][2] *
-                     new_mat[0][1] -
-                     (new_mat[0][0] - 1) *
-                     new_mat[0][3]))
+        self.r1 = (
+            (1 / self.area)
+            * ((new_mat[3][3] - 1) * new_mat[0][2] + new_mat[2][3] * new_mat[0][3])
+            / ((new_mat[3][3] - 1) ** 2 + new_mat[2][3] ** 2)
+        )
+        self.r2 = (
+            (1 / self.area)
+            * ((new_mat[0][0] - 1) * new_mat[0][2] + new_mat[0][1] * new_mat[0][3])
+            / ((new_mat[0][0] - 1) ** 2 + new_mat[0][1] ** 2)
+        )
+        self.c1 = (
+            self.area
+            * ((new_mat[3][3] - 1) ** 2 + (new_mat[2][3]) ** 2)
+            / (
+                omega
+                * (new_mat[0][2] * new_mat[2][3] - (new_mat[3][3] - 1) * new_mat[0][3])
+            )
+        )
+        self.c2 = (
+            self.area
+            * ((new_mat[0][0] - 1) ** 2 + (new_mat[0][1]) ** 2)
+            / (
+                omega
+                * (new_mat[0][2] * new_mat[0][1] - (new_mat[0][0] - 1) * new_mat[0][3])
+            )
+        )
         self.r3 = (1 / self.area) * (np.sum(r_layer)) - self.r1 - self.r2
 
         r_wall = self.r1 + self.r2 + self.r3
 
-        self.c1_korr = (1 / (omega * self.r1)) * ((r_wall * self.area -
-                                                   new_mat[0][2] * new_mat[3][
-                                                       3] -
-                                                   new_mat[0][3] * new_mat[2][
-                                                       3]) /
-                                                  (new_mat[3][3] * new_mat[0][
-                                                      3] -
-                                                   new_mat[0][2] * new_mat[2][
-                                                       3]))
+        self.c1_korr = (1 / (omega * self.r1)) * (
+            (
+                r_wall * self.area
+                - new_mat[0][2] * new_mat[3][3]
+                - new_mat[0][3] * new_mat[2][3]
+            )
+            / (new_mat[3][3] * new_mat[0][3] - new_mat[0][2] * new_mat[2][3])
+        )
 
-        if type(self).__name__ == "OuterWall" \
-                or type(self).__name__ == "Rooftop" \
-                or type(self).__name__ == "GroundFloor":
+        if (
+            type(self).__name__ == "OuterWall"
+            or type(self).__name__ == "Rooftop"
+            or type(self).__name__ == "GroundFloor"
+        ):
             self.c1 = self.c1_korr
 
-    def insulate_wall(
-            self,
-            material=None,
-            thickness=None):
+    def insulate_wall(self, material=None, thickness=None):
         """Retrofit the walls with an additional insulation layer
 
         Adds an additional layer on the wall, outer sight
@@ -249,8 +277,8 @@ class Wall(BuildingElement):
         ext_layer = Layer(self)
         new_material = Material(ext_layer)
         new_material.load_material_template(
-            material,
-            data_class=self.parent.parent.parent.data)
+            material, data_class=self.parent.parent.parent.data
+        )
 
         if thickness is None:
             pass
@@ -290,11 +318,13 @@ class Wall(BuildingElement):
 
         if year_of_retrofit < 1977:
             year_of_retrofit = 1977
-            warnings.warn("You are using a year of retrofit not supported\
+            warnings.warn(
+                "You are using a year of retrofit not supported\
                     by teaser. We will change your year of retrofit to 1977\
-                    for the calculation. Be careful!")
+                    for the calculation. Be careful!"
+            )
 
-        if type(self).__name__ == 'OuterWall':
+        if type(self).__name__ == "OuterWall":
 
             if 1977 <= year_of_retrofit <= 1981:
                 self.insulate_wall(material)
@@ -311,11 +341,14 @@ class Wall(BuildingElement):
             elif 2009 <= year_of_retrofit <= 2013:
                 self.insulate_wall(material)
                 calc_u = 0.24 * self.area
-            elif year_of_retrofit >= 2014:
+            elif 2014 <= year_of_retrofit <= 2015:
+                self.insulate_wall(material)
+                calc_u = 0.24 * self.area
+            elif year_of_retrofit >= 2016:
                 self.insulate_wall(material)
                 calc_u = 0.24 * self.area
 
-        elif type(self).__name__ == 'Rooftop':
+        elif type(self).__name__ == "Rooftop":
 
             if 1977 <= year_of_retrofit <= 1981:
                 self.insulate_wall(material)
@@ -332,11 +365,14 @@ class Wall(BuildingElement):
             elif 2009 <= year_of_retrofit <= 2013:
                 self.insulate_wall(material)
                 calc_u = 0.2 * self.area
-            elif year_of_retrofit >= 2014:
+            elif 2014 <= year_of_retrofit <= 2015:
                 self.insulate_wall(material)
                 calc_u = 0.2 * self.area
+            elif year_of_retrofit >= 2016:
+                self.insulate_wall(material)
+                calc_u = 0.14 * self.area
 
-        if type(self).__name__ == 'GroundFloor':
+        if type(self).__name__ == "GroundFloor":
 
             if 1977 <= year_of_retrofit <= 1981:
                 self.insulate_wall(material)
@@ -353,9 +389,12 @@ class Wall(BuildingElement):
             elif 2009 <= year_of_retrofit <= 2013:
                 self.insulate_wall(material)
                 calc_u = 0.3 * self.area
-            elif year_of_retrofit >= 2014:
+            elif 2014 <= year_of_retrofit <= 2015:
                 self.insulate_wall(material)
                 calc_u = 0.3 * self.area
+            elif year_of_retrofit >= 2016:
+                self.insulate_wall(material)
+                calc_u = 0.25 * self.area
 
         r_conduc = 0
 
@@ -363,14 +402,15 @@ class Wall(BuildingElement):
             pass
         else:
             for count_layer in self.layer[:-1]:
-                r_conduc += (count_layer.thickness /
-                             count_layer.material.thermal_conduc)
+                r_conduc += count_layer.thickness / count_layer.material.thermal_conduc
 
-                self.layer[-1].thickness = \
-                    (((
-                      1 - calc_u * self.r_inner_comb - calc_u *
-                      self.r_outer_comb) /
-                      calc_u) * self.area - r_conduc) * \
-                    self.layer[-1].material.thermal_conduc
+                self.layer[-1].thickness = (
+                    (
+                        (1 - calc_u * self.r_inner_comb - calc_u * self.r_outer_comb)
+                        / calc_u
+                    )
+                    * self.area
+                    - r_conduc
+                ) * self.layer[-1].material.thermal_conduc
 
                 self.layer[-1].id = len(self.layer)
