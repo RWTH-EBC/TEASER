bdaea26e725898e547c0a6acfcce47c077cad3dd
Christian Wenzel <ctwenzel@yahoo.de>
2021-05-25 19:53:52 +0200

    script to calculate and plot simulation results

---
+++ teaser/examples/utils/MA_cwe_calculate_KPIs_and_plot_results.py
@@ -0,0 +1,38 @@
+# Specify path to your simulation workspace
+import os
+import datetime
+import read_results as res
+import pandas as pd
+from teaser.project import Project
+import pickle
+
+if __name__ == '__main__':
+
+    workspace = os.path.join("D:\\", "tbl-cwe", "Building_simulation")
+    print("Your workspace is set to: ")
+    print(workspace)
+
+    load_pickle = os.path.join(workspace, "building_simulation_pickle.p")
+
+    pickle_prj = pickle.load(open(load_pickle, "rb"))
+    print(pickle_prj)
+
+    output_path = os.path.join(workspace, "calc_results")
+    print("Your Calculation Results are stored in: ")
+    print(output_path)
+    plot_path = os.path.join(workspace, "plots")
+    print("Your Plots are stored in: ")
+    print(plot_path)
+
+    #for bldg in pickle_prj.buildings:
+    res.calc_results(
+        buildings=pickle_prj.buildings,
+        csv_path=os.path.join(workspace, "csv_results",),
+        output_path=os.path.join(output_path),
+        plot_path=os.path.join(plot_path))
+
+    """res.plot_results(
+        buildings=pickle_prj.buildings,
+        csv_path=os.path.join(workspace, "csv_results",),
+        output_path=os.path.join(plot_path))"""
+    #bldg.name
