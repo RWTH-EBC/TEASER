1d4f53fd4722063d5f72d5a563988a606c193998
Christian Wenzel <ctwenzel@yahoo.de>
2021-05-31 01:23:19 +0200

    deleted unused code

--- teaser/examples/utils/MA_cwe_calculate_KPIs_and_plot_results.py
+++ teaser/examples/utils/MA_cwe_calculate_KPIs_and_plot_results.py
@@ -4,24 +4,22 @@ import pickle
 import read_results as res
 
 if __name__ == '__main__':
+    
     # set path to your workspace here
     workspace = os.path.join("D:\\", "tbl-cwe", "Simulationsstudie_06_21")
     print("Your workspace is set to: " + workspace)
-    # print(workspace)
 
     load_pickle = os.path.join(workspace, "building_simulation_pickle.p")
     pickle_prj = pickle.load(open(load_pickle, "rb"))
 
     output_path = os.path.join(workspace, "calc_results")
-    print("Your Calculation Results are stored in: " + output_path)
-    # print(output_path)
+    print("Your calculation results are stored in: " + output_path)
 
     plot_path = os.path.join(workspace, "plots")
-    print("Your Plots are stored in: " + plot_path)
-    # print(plot_path)
+    print("Your plots are stored in: " + plot_path)
 
     csv_results_path = os.path.join(workspace, "csv_results", )
-    print("Your Plots are stored in: " + csv_results_path)
+    print("Your .csv  files are stored in: " + csv_results_path)
 
     print("##########")
     res.calc_results(
