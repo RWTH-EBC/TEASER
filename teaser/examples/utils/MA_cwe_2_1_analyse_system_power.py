b4ea5418c794e39866d8638b37d2ab891ab1b71d
Christian Wenzel <ctwenzel@yahoo.de>
2021-06-10 19:06:56 +0200

    scripts to analyse system power

--- teaser/examples/utils/MA_cwe_2_1_analyse_system_power.py
+++ teaser/examples/utils/MA_cwe_2_1_analyse_system_power.py
@@ -1,13 +1,115 @@
 import os
 import datetime
-import read_results as res
+#import read_results as res
 import pandas as pd
 import pickle
+from dymola.dymola_interface import DymolaInterface
+
+def read_results_power(
+        buildings,
+        signals,
+        index=pd.date_range(datetime.datetime(2021, 1, 1), periods=1, freq="H"),
+        index_res=pd.date_range(datetime.datetime(2021, 1, 1), periods=1, freq="H"),
+        results_path=None,
+        csv_path=None,
+):
+    """Read simulation data from .mat file and save them into csv.
+
+    Reads Dymola result files and saves them as time series in csv. It assumes that all
+    thermal_zones are a MultiZone (TEASER Export) in Modelica.
+
+    Parameters
+    ----------
+    buildings : list
+        List of TEASER bldgs
+    signals : list
+        List of signals to be read from the results file.
+    index : Pandas date_range
+        Pandas date range of the simulation data. Must fit the length of
+        simulation data. (Beware of leap years! every 4 years, e.g. 2020) (default: hourly for year 2019)
+    index_res : Pandas date_range
+        Pandas date range  if slice should be picked e. g. only january of the simulation data. Must fit the length of
+        simulation data. (default: hourly for year 2015)
+    results_path : str
+        Path where Dymola results are  stored.
+    csv_path : str
+        Path where csv results should be stored.
+
+    """
+
+    if not os.path.exists(csv_path):
+        os.makedirs(csv_path)
+    dymola = DymolaInterface()
+
+    for bldg in buildings:
+        print("reading building {}".format(bldg))
+        # bldg.name # bldg
+        #if not (bldg + "_heat.csv") in os.listdir(csv_path):
+        try:
+            #print(os.path.join(results_path, bldg + ".mat"))
+            dym_res = dymola.readTrajectory(
+                fileName=os.path.join(results_path, bldg + ".mat"),
+                signals=signals,
+                rows=dymola.readTrajectorySize(
+                    fileName=os.path.join(results_path, bldg + ".mat")
+                ),
+            )
+            results = pd.DataFrame().from_records(dym_res).T
+            results = results.rename(
+                columns=dict(zip(results.columns.values, signals))
+            )
+        except BaseException:
+            # Dymola has strange exceptions
+            print(
+                "Reading results of building {} failed, "
+                "please check result file".format(bldg)
+            )
+            # raise Exception("Results Error!")
+            continue
+        try:
+            results.index = index
+        except ValueError:
+            print(
+                "Simulation results of building {} are most likely "
+                "faulty (series is shorter then one year), please check "
+                "result file".format(bldg)
+            )
+            # raise Exception("Completion Error!")
+            continue
+
+        power = pd.DataFrame(
+            data=results.filter(like="hHeatPanel").sum(axis=1)[
+                 index_res[0]: index_res[-1]
+                 ],
+            index=index_res,
+            columns=[bldg + " hHeatPanel"],
+        )
+        power.loc[:, bldg + " lCoolPanel"] = results.filter(like="lCoolPanel").sum(axis=1)[
+                                                  index_res[0]: index_res[-1]
+                                                  ]
+        power.loc[:, bldg + " powerHeatTabs"] = results.filter(like="powerHeatTabs").sum(axis=1)[
+                                                 index_res[0]: index_res[-1]
+                                                 ]
+        power.loc[:, bldg + " powerCoolTabs"] = results.filter(like="powerCoolTabs").sum(axis=1)[
+                                                   index_res[0]: index_res[-1]
+                                                   ]
+        power.loc[:, bldg + " hHeatRem"] = results.filter(like="hHeatRem").sum(axis=1)[
+                                                index_res[0]: index_res[-1]
+                                                ]
+        power.loc[:, bldg + " lCoolRem"] = results.filter(like="lCoolRem").sum(axis=1)[
+                                                index_res[0]: index_res[-1]
+                                                ]
+
+        power.to_csv(os.path.join(csv_path, bldg + "_power.csv"))
+        #power.to_excel(os.path.join(csv_path, bldg + "_power.xlsx"))
+
+    dymola.close()
+
 
 if __name__ == '__main__':
 
     # set path to your workspace here
-    workspace = os.path.join("D:\\", "tbl-cwe", "Simulationsstudie_06_21_der_T_adjust")
+    workspace = os.path.join("D:\\", "tbl-cwe", "TABS_power", "Simulationsstudie_TABS_50")
     print("Your workspace is set to: " + workspace)
 
     """
@@ -31,22 +133,16 @@ if __name__ == '__main__':
     sim_results_path = os.path.join(workspace, "sim_results", pickle_prj.name)
     print("Your Dymola Results are stored in: " + sim_results_path)
 
-    csv_results_path = os.path.join(workspace, "csv_results")
+    csv_results_path = os.path.join(workspace, "csv_results", "power_comparison")
     print("Your .csv files are stored in: " + csv_results_path)
     print("##########")
 
     for bldg in pickle_prj.buildings:
         if "panel" in bldg.name:
-            signals = ['multizone.TOpe[{}]'.format(i + 1) for i in range(len(bldg.thermal_zones))]
-            signals += ['multizone.TAir[{}]'.format(i + 1) for i in range(len(bldg.thermal_zones))]
-            signals += ['multizone.zone[{}].heaterCoolerWithTabs6007C1.pITempHeatPanel.y'.format(i + 1) for i in
-                        range(len(bldg.thermal_zones))]
-            signals += ['multizone.zone[{}].heaterCoolerWithTabs6007C1.pITempCoolPanel.y'.format(i + 1) for i in
-                        range(len(bldg.thermal_zones))]
-            signals += ['multizone.PHeater[{}]'.format(i + 1) for i in range(len(bldg.thermal_zones))]
-            signals += ['multizone.PCooler[{}]'.format(i + 1) for i in range(len(bldg.thermal_zones))]
+            signals = ['multizone.zone[{}].zoneParam.hHeatPanel'.format(i + 1) for i in range(len(bldg.thermal_zones))]
+            signals += ['multizone.zone[{}].zoneParam.lCoolPanel'.format(i + 1) for i in range(len(bldg.thermal_zones))]
 
-            res.read_results(
+            read_results_power(
                 buildings=[bldg.name],
                 signals=signals,
                 index=pd.date_range(datetime.datetime(2021, 1, 1), periods=8760, freq="H"),
@@ -55,20 +151,12 @@ if __name__ == '__main__':
                 csv_path=csv_results_path,
             )
         elif "tabsplusair" in bldg.name:
-            signals = ['multizone.TOpe[{}]'.format(i + 1) for i in range(len(bldg.thermal_zones))]
-            signals += ['multizone.TAir[{}]'.format(i + 1) for i in range(len(bldg.thermal_zones))]
-            signals += ['multizone.zone[{}].heaterCoolerWithTabs6007C1.tabsHeatingPower'.format(i + 1) for i in
-                        range(len(bldg.thermal_zones))]
-            signals += ['multizone.zone[{}].heaterCoolerWithTabs6007C1.tabsCoolingPower'.format(i + 1) for i in
-                        range(len(bldg.thermal_zones))]
-            signals += ['multizone.zone[{}].heaterCoolerWithTabs6007C1.pITempHeatRem.y'.format(i + 1) for i in
-                        range(len(bldg.thermal_zones))]
-            signals += ['multizone.zone[{}].heaterCoolerWithTabs6007C1.pITempCoolRem.y'.format(i + 1) for i in
-                        range(len(bldg.thermal_zones))]
-            signals += ['multizone.PHeater[{}]'.format(i + 1) for i in range(len(bldg.thermal_zones))]
-            signals += ['multizone.PCooler[{}]'.format(i + 1) for i in range(len(bldg.thermal_zones))]
+            signals = ['multizone.zone[{}].zoneParam.powerHeatTabs'.format(i + 1) for i in range(len(bldg.thermal_zones))]
+            signals += ['multizone.zone[{}].zoneParam.powerCoolTabs'.format(i + 1) for i in range(len(bldg.thermal_zones))]
+            signals += ['multizone.zone[{}].zoneParam.hHeatRem'.format(i + 1) for i in range(len(bldg.thermal_zones))]
+            signals += ['multizone.zone[{}].zoneParam.lCoolRem'.format(i + 1) for i in range(len(bldg.thermal_zones))]
 
-            res.read_results(
+            read_results_power(
                 buildings=[bldg.name],
                 signals=signals,
                 index=pd.date_range(datetime.datetime(2021, 1, 1), periods=8760, freq="H"),
@@ -77,13 +165,10 @@ if __name__ == '__main__':
                 csv_path=csv_results_path,
             )
         elif "radiator" in bldg.name:
-            signals = ['multizone.TOpe[{}]'.format(i + 1) for i in range(len(bldg.thermal_zones))]
-            signals += ['multizone.TAir[{}]'.format(i + 1) for i in range(len(bldg.thermal_zones))]
-            signals += ['multizone.zone[{}].heaterCoolerWithTabs6007C1.pITempHeatRem.y'.format(i + 1) for i in
-                        range(len(bldg.thermal_zones))]
-            signals += ['multizone.PHeater[{}]'.format(i + 1) for i in range(len(bldg.thermal_zones))]
+            signals = ['multizone.zone[{}].zoneParam.hHeatRem'.format(i + 1) for i in range(len(bldg.thermal_zones))]
+            signals += ['multizone.zone[{}].zoneParam.lCoolRem'.format(i + 1) for i in range(len(bldg.thermal_zones))]
 
-            res.read_results(
+            read_results_power(
                 buildings=[bldg.name],
                 signals=signals,
                 index=pd.date_range(datetime.datetime(2021, 1, 1), periods=8760, freq="H"),
@@ -92,16 +177,10 @@ if __name__ == '__main__':
                 csv_path=csv_results_path,
             )
         elif "convective" in bldg.name:
-            signals = ['multizone.TOpe[{}]'.format(i + 1) for i in range(len(bldg.thermal_zones))]
-            signals += ['multizone.TAir[{}]'.format(i + 1) for i in range(len(bldg.thermal_zones))]
-            signals += ['multizone.zone[{}].heaterCoolerWithTabs6007C1.pITempHeatRem.y'.format(i + 1) for i in
-                        range(len(bldg.thermal_zones))]
-            signals += ['multizone.zone[{}].heaterCoolerWithTabs6007C1.pITempCoolRem.y'.format(i + 1) for i in
-                        range(len(bldg.thermal_zones))]
-            signals += ['multizone.PHeater[{}]'.format(i + 1) for i in range(len(bldg.thermal_zones))]
-            signals += ['multizone.PCooler[{}]'.format(i + 1) for i in range(len(bldg.thermal_zones))]
+            signals = ['multizone.zone[{}].zoneParam.hHeatRem'.format(i + 1) for i in range(len(bldg.thermal_zones))]
+            signals += ['multizone.zone[{}].zoneParam.lCoolRem'.format(i + 1) for i in range(len(bldg.thermal_zones))]
 
-            res.read_results(
+            read_results_power(
                 buildings=[bldg.name],
                 signals=signals,
                 index=pd.date_range(datetime.datetime(2021, 1, 1), periods=8760, freq="H"),
@@ -109,13 +188,5 @@ if __name__ == '__main__':
                 results_path=sim_results_path,
                 csv_path=csv_results_path,
             )
-        """res.read_results(
-            buildings=pickle_prj.buildings,
-            signals=signals,
-            index=pd.date_range(datetime.datetime(2021, 1, 1), periods=8760, freq="H"),
-            index_res=pd.date_range(datetime.datetime(2021, 1, 1), periods=8760, freq="H"),
-            results_path=sim_results_path,
-            csv_path=csv_results_path,
-        )"""
 
     print("That's it! :)")
--- teaser/examples/utils/MA_cwe_3_1_power_to_xlsx.py
+++ teaser/examples/utils/MA_cwe_3_1_power_to_xlsx.py
@@ -1,12 +1,121 @@
 import os
 import pickle
+import pandas as pd
 
 import read_results as res
+
+def calc_power(buildings, csv_path, output_path):
+    """Create csv-file containing KPIs of buildings.
+
+    Parameters
+    ----------
+    buildings : prj.buildings
+        load TEASER bldgs from a pickle project file
+    csv_path : str
+        Path where hourly demands created by MA_cwe_2_analyse_results.py are stored
+    output_path : str
+        Path where output files should be stored
+
+    """
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    results = pd.DataFrame()
+
+    for bldg in buildings:
+        try:
+            print("reading building {}".format(bldg.name))
+            # read hourly demands
+            power_data = pd.read_csv(os.path.join(csv_path, bldg.name + "_power.csv"))
+
+            # calculate annual heating and cooling demand
+            power_heat_tabs = power_data.loc[:, bldg.name + " powerHeatTabs"].max() / 1000
+            power_cool_tabs = power_data.loc[:, bldg.name + " powerCoolTabs"].max() / 1000
+            power_heat_panel = power_data.loc[:, bldg.name + " hHeatPanel"].max() / 1000
+            power_cool_panel = power_data.loc[:, bldg.name + " lCoolPanel"].max() / 1000
+            power_heat_rem = power_data.loc[:, bldg.name + " hHeatRem"].max() / 1000
+            power_cool_rem = power_data.loc[:, bldg.name + " lCoolRem"].max() / 1000
+            power_heat_tabsplus = (power_data.loc[:, bldg.name + " powerHeatTabs"].max() + power_data.loc[:, bldg.name + " hHeatRem"].max()) / 1000
+            power_cool_tabsplus = (power_data.loc[:, bldg.name + " powerCoolTabs"].max() + power_data.loc[:, bldg.name + " lCoolRem"].max()) / 1000
+
+            # calculate specific annual heating and cooling demand
+            spec_power_heat_tabs = power_heat_tabs *1000 / bldg.net_leased_area
+            spec_power_cool_tabs = power_cool_tabs *1000 / bldg.net_leased_area
+            spec_power_heat_panel = power_heat_panel *1000 / bldg.net_leased_area
+            spec_power_cool_panel = power_cool_panel *1000 / bldg.net_leased_area
+            spec_power_heat_rem = power_heat_rem *1000 / bldg.net_leased_area
+            spec_power_cool_rem = power_cool_rem *1000 / bldg.net_leased_area
+            spec_power_heat_tabsplus = power_heat_tabsplus * 1000 / bldg.net_leased_area
+            spec_power_cool_tabsplus = power_cool_tabsplus * 1000 / bldg.net_leased_area
+
+            if "EFH" in bldg.name:
+                results.loc[bldg.name, "Type"] = "EFH"
+            elif "MFH" in bldg.name:
+                results.loc[bldg.name, "Type"] = "MFH"
+            elif "Office" in bldg.name:
+                results.loc[bldg.name, "Type"] = "Office"
+            if "radiator" in bldg.name:
+                results.loc[bldg.name, "System"] = "Radiator"
+            elif "convective" in bldg.name:
+                results.loc[bldg.name, "System"] = "Convective"
+            elif "panel" in bldg.name:
+                results.loc[bldg.name, "System"] = "Panel"
+            elif "tabsplusair" in bldg.name:
+                results.loc[bldg.name, "System"] = "TABSplusAir"
+
+            results.loc[bldg.name, "Year of construction"] = bldg.year_of_construction
+            results.loc[bldg.name, "Net area"] = bldg.net_leased_area
+            #results.loc[bldg.name, "Footprint area"] = bldg.ground_floor_geo["GroundFloor"]["area"]
+            #results.loc[bldg.name, "Volume"] = bldg.volume
+            results.loc[bldg.name, "A/V"] = bldg.library_attr.total_surface_area / bldg.volume
+            results.loc[bldg.name, "Construction type"] = bldg.construction_type
+
+            if "radiator" in bldg.name:
+                results.loc[bldg.name, "Heating Power Rem [kW]"] = round(power_heat_rem, 1)
+                results.loc[bldg.name, "Specific Heating Power Rem [W/sqm]"] = round(spec_power_heat_rem, 1)
+            elif "convective" in bldg.name:
+                results.loc[bldg.name, "Heating Power Rem [kW]"] = round(power_heat_rem, 1)
+                results.loc[bldg.name, "Specific Heating Power Rem [W/sqm]"] = round(spec_power_heat_rem, 1)
+                results.loc[bldg.name, "Cooling Power Rem [kW]"] = round(power_cool_rem, 1)
+                results.loc[bldg.name, "Specific Cooling Power Rem [W/sqm]"] = round(spec_power_cool_rem, 1)
+            elif "panel" in bldg.name:
+                results.loc[bldg.name, "Heating Power Panel [kW]"] = round(power_heat_panel, 1)
+                results.loc[bldg.name, "Specific Heating Power Panel [W/sqm]"] = round(spec_power_heat_panel, 1)
+                results.loc[bldg.name, "Cooling Power Panel [kW]"] = round(power_cool_panel, 1)
+                results.loc[bldg.name, "Specific Cooling Power Panel [W/sqm]"] = round(spec_power_cool_panel, 1)
+            elif "tabsplusair" in bldg.name:
+                results.loc[bldg.name, "Heating Power TABS [kW]"] = round(power_heat_tabs, 1)
+                results.loc[bldg.name, "Specific Heating Power TABS [W/sqm]"] = round(spec_power_heat_tabs, 1)
+                results.loc[bldg.name, "Cooling Power TABS [kW]"] = round(power_cool_tabs, 1)
+                results.loc[bldg.name, "Specific Cooling Power TABS [W/sqm]"] = round(spec_power_cool_tabs, 1)
+                results.loc[bldg.name, "Heating Power Rem [kW]"] = round(power_heat_rem, 1)
+                results.loc[bldg.name, "Specific Heating Power Rem [W/sqm]"] = round(spec_power_heat_rem, 1)
+                results.loc[bldg.name, "Cooling Power Rem [kW]"] = round(power_cool_rem, 1)
+                results.loc[bldg.name, "Specific Cooling Power Rem [W/sqm]"] = round(spec_power_cool_rem, 1)
+                results.loc[bldg.name, "Heating Power TABSplusAir [kW]"] = round(power_heat_tabsplus, 1)
+                results.loc[bldg.name, "Specific Heating Power TABSplusAir [W/sqm]"] = round(spec_power_heat_tabsplus, 1)
+                results.loc[bldg.name, "Cooling Power TABSplusAir [kW]"] = round(power_cool_tabsplus, 1)
+                results.loc[bldg.name, "Specific Cooling Power TABSplusAir [W/sqm]"] = round(spec_power_cool_tabsplus, 1)
+
+
+        except BaseException:
+            # Dymola has strange exceptions
+            print(
+                "Reading results of building {} failed, "
+                "please check result file".format(bldg.name)
+            )
+            # raise Exception("Results Error!")
+            continue
+
+    results.to_csv(os.path.join(output_path, "systempower_calc.csv"))
+    results.to_excel(os.path.join(output_path, "systempower_excel_calc.xlsx"))
+    print("Calculations done! :)")
 
 if __name__ == '__main__':
 
     # set path to your workspace here
-    workspace = os.path.join("D:\\", "tbl-cwe", "Simulationsstudie_06_21_der_T_adjust")
+    workspace = os.path.join("D:\\", "tbl-cwe", "TABS_power", "Simulationsstudie_TABS_50")
     print("Your workspace is set to: " + workspace)
 
     load_pickle = os.path.join(workspace, "building_simulation_pickle.p")
@@ -15,20 +124,11 @@ if __name__ == '__main__':
     output_path = os.path.join(workspace, "calc_results")
     print("Your calculation results are stored in: " + output_path)
 
-    plot_path = os.path.join(workspace, "plots")
-    print("Your plots are stored in: " + plot_path)
-
-    csv_results_path = os.path.join(workspace, "csv_results", )
+    csv_results_path = os.path.join(workspace, "csv_results", "power_comparison")
     print("Your .csv  files are stored in: " + csv_results_path)
 
     print("##########")
-    res.calc_results(
+    calc_power(
         buildings=pickle_prj.buildings,
         csv_path=csv_results_path,
         output_path=output_path)
-
-    print("##########")
-    res.plot_results(
-        buildings=pickle_prj.buildings,
-        csv_path=csv_results_path,
-        output_path=plot_path)
