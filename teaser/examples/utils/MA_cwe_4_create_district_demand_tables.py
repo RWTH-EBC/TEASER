dc5b54af0015906f0379a4ebf5abb49a52caa71c
Christian Wenzel <ctwenzel@yahoo.de>
2021-08-04 10:15:45 +0200

    add exception handling

--- teaser/examples/utils/MA_cwe_4_create_district_demand_tables.py
+++ teaser/examples/utils/MA_cwe_4_create_district_demand_tables.py
@@ -13,7 +13,7 @@ Use this script to create heating and cooling demand tables for DHC network simu
 if __name__ == '__main__':
     timing
     # set your workspace to your desired path here
-    workspace = os.path.join("D:\\", "tbl-cwe", "Final_Simulations", "Data_for_Network_Radiator_Convective_24_07")
+    workspace = os.path.join("D:\\", "tbl-cwe", "Final_Simulations", "Complete_04_08")
     sim_results_path = os.path.join(workspace, "sim_results")
     TEASER_output_path = os.path.join(workspace, "TEASEROutput")
 
@@ -37,52 +37,62 @@ if __name__ == '__main__':
     print("Creating demand tables")
 
     for bldg in pickle_prj.buildings:
-        ma = pd.DataFrame()
-        co = pd.DataFrame()
-        ma_bldg = pd.DataFrame()
-        co_bldg = pd.DataFrame()
-        ma = pd.read_csv(os.path.join(csv_results_path, bldg.name + "_heat.csv"))
-        co = pd.read_csv(os.path.join(csv_results_path, bldg.name + "_cool.csv"))
+        try:
+            ma = pd.DataFrame()
+            co = pd.DataFrame()
+            ma_bldg = pd.DataFrame()
+            co_bldg = pd.DataFrame()
+            ma = pd.read_csv(os.path.join(csv_results_path, bldg.name + "_heat.csv"))
+            co = pd.read_csv(os.path.join(csv_results_path, bldg.name + "_cool.csv"))
 
-        i = 0
+            i = 0
 
-        for column in ma.columns[1:]:
-            ma.to_csv(os.path.join(dhc_demands_path, column + "_ma.txt"), sep ='\t')
-            ma.index = [i * 3600 for i in range(8760)]
-            ma_bldg = ma[column]
+            for column in ma.columns[1:]:
+                ma.to_csv(os.path.join(dhc_demands_path, column + "_ma.txt"), sep ='\t')
+                ma.index = [i * 3600 for i in range(8760)]
+                ma_bldg = ma[column]
 
-            # ma_bldg = ma_bldg.drop(ma_bldg.index[0:24])
-            ma_bldg.to_csv(os.path.join(dhc_demands_path, column + "_ma.txt"), header=False, sep ='\t')
+                # ma_bldg = ma_bldg.drop(ma_bldg.index[0:24])
+                ma_bldg.to_csv(os.path.join(dhc_demands_path, column + "_ma.txt"), header=False, sep ='\t')
 
-            with open(os.path.join(dhc_demands_path, column + "_ma.txt")) as f:
-                lines = f.readlines()
+                with open(os.path.join(dhc_demands_path, column + "_ma.txt")) as f:
+                    lines = f.readlines()
 
-            lines.insert(0, "#1\n")
-            lines.insert(1, 'double heatDemand(8760,2)\n')
-            # lines.insert(1, 'double heat(8736,2)\n')
+                lines.insert(0, "#1\n")
+                lines.insert(1, 'double heatDemand(8760,2)\n')
+                # lines.insert(1, 'double heat(8736,2)\n')
 
-            with open(os.path.join(dhc_demands_path, column + "_ma.txt"), "w") as f:
-                f.writelines(lines)
+                with open(os.path.join(dhc_demands_path, column + "_ma.txt"), "w") as f:
+                    f.writelines(lines)
 
-            f.close()
+                f.close()
 
-        for column in co.columns[1:]:
-            co.to_csv(os.path.join(dhc_demands_path, column + "_co.txt"), sep ='\t')
-            co.index = [i * 3600 for i in range(8760)]
-            co_bldg = co[column]
+            for column in co.columns[1:]:
+                co.to_csv(os.path.join(dhc_demands_path, column + "_co.txt"), sep ='\t')
+                co.index = [i * 3600 for i in range(8760)]
+                co_bldg = co[column]
 
-            # ma_bldg = ma_bldg.drop(ma_bldg.index[0:24])
-            co_bldg.to_csv(os.path.join(dhc_demands_path, column + "_co.txt"), header=False, sep ='\t')
+                # ma_bldg = ma_bldg.drop(ma_bldg.index[0:24])
+                co_bldg.to_csv(os.path.join(dhc_demands_path, column + "_co.txt"), header=False, sep ='\t')
 
-            with open(os.path.join(dhc_demands_path, column + "_co.txt")) as f:
-                lines = f.readlines()
+                with open(os.path.join(dhc_demands_path, column + "_co.txt")) as f:
+                    lines = f.readlines()
 
-            lines.insert(0, "#1\n")
-            lines.insert(1, 'double coolingDemand(8760,2)\n')
-            # lines.insert(1, 'double heat(8736,2)\n')
+                lines.insert(0, "#1\n")
+                lines.insert(1, 'double coolingDemand(8760,2)\n')
+                # lines.insert(1, 'double heat(8736,2)\n')
 
-            with open(os.path.join(dhc_demands_path, column + "_co.txt"), "w") as f:
-                f.writelines(lines)
+                with open(os.path.join(dhc_demands_path, column + "_co.txt"), "w") as f:
+                    f.writelines(lines)
 
-            f.close()
+                f.close()
+
+        except BaseException:
+            # Dymola has strange exceptions
+            print(
+                "Reading results of building {} failed, "
+                "please check result file".format(bldg)
+            )
+            # raise Exception("Results Error!")
+            continue
 
