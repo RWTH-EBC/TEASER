32b6a8bab966e5aff233d37c4bcba765d554b749
diego.cordoba <diego.cordoba@eonerc.rwth-aachen.de>
2021-09-16 23:29:48 +0200

    change on .mat extraction

--- teaser/UFH_Integration/mat_extraction.py
+++ teaser/UFH_Integration/mat_extraction.py
@@ -8,6 +8,7 @@ from os import walk
 
 def main(with_plot=True):
     variables_building = ['ROM.intGainsConv.T', 'ROM.intGainsConv.Q_flow', 'ROM.intGainsRad.T', 'ROM.intGainsRad.Q_flow']
+    variables_discr = ['vol2.T', 'vol2.heatPort.Q_flow', 'vol1.T', 'vol2.heatPort.Q_flow']
     zones = {'as Walls': 'blue', 'as Tabs': 'red'}
     basepath = pathlib.Path(__file__).parents[4].joinpath("Dymola_results")
     result_files = next(walk(basepath))[2]
@@ -15,6 +16,7 @@ def main(with_plot=True):
     for result_file in result_files:
         RMS[result_file] = {}
         if result_file.startswith('SimpleBuilding') and result_file.endswith('.mat'):
+            continue
             tsd_mat = TimeSeriesData(basepath.joinpath(result_file)).to_df()
             for variable in variables_building:
                 plt.figure()
@@ -40,6 +42,42 @@ def main(with_plot=True):
                 if with_plot:
                     plt.show()
                 RMS[result_file][variable] = math.sqrt(sum(rms)/len(rms))
+        if result_file.startswith('Discretisation') and result_file.endswith('.mat'):
+            tsd_mat = TimeSeriesData(basepath.joinpath(result_file)).to_df()
+            plt.figure()
+            fig, axs = plt.subplots(2)
+            axs[0].plot(tsd_mat[variables_discr[0]], label=1, color='blue')
+            axs[0].plot(tsd_mat[variables_discr[2]], label=2, color='red')
+            axs[0].legend()
+            fig.suptitle(result_file + 'vol temperature')
+            # error
+            # Relative Percent Difference
+            # error = 2 * abs((tsd_mat["multizone.zone[1].%s" % variable] - tsd_mat["multizone.zone[2].%s" % variable]) /
+            #                 (tsd_mat["multizone.zone[1].%s" % variable] + tsd_mat["multizone.zone[2].%s" % variable]))
+            # relative error
+            error = abs(tsd_mat[variables_discr[0]] - tsd_mat[variables_discr[2]]) / \
+                    abs(1 + tsd_mat[variables_discr[0]])
+            axs[1].plot(error, label="Relative Percent Difference", color="blue")
+            axs[1].legend()
+            # RMS
+            # rms = (tsd_mat["multizone.zone[1].%s" % variable] - tsd_mat["multizone.zone[2].%s" % variable]) ** 2
+            if with_plot:
+                plt.show()
+            # RMS[result_file][variable] = math.sqrt(sum(rms) / len(rms))
+            plt.figure()
+            fig, axs = plt.subplots(2)
+            axs[0].plot(tsd_mat[variables_discr[1]], label=1, color='blue')
+            axs[0].plot(tsd_mat[variables_discr[3]], label=2, color='red')
+            axs[0].legend()
+            fig.suptitle(result_file + 'vol temperature')
+
+            error = abs(tsd_mat[variables_discr[1]] - tsd_mat[variables_discr[3]]) / \
+                    abs(1 + tsd_mat[variables_discr[1]])
+            axs[1].plot(error, label="Relative Percent Difference", color="blue")
+            axs[1].legend()
+            if with_plot:
+                plt.show()
+
     pass
 
 
