21b95bfed1829433c6afaeac2cef3f32f0b2adf2
diego.cordoba <diego.cordoba@eonerc.rwth-aachen.de>
2021-09-18 18:59:45 +0200

    started tests building in teaser

--- teaser/UFH_Integration/export_all.py
+++ teaser/UFH_Integration/export_all.py
@@ -25,36 +25,52 @@ class ExportAll:
         self.ufh_instances = []
 
     def run(self):
-        name = 'testtabs1'
+        # name = 'testtabs1'
+        # prj = self.create_project(name)
+        # prj.modelica_info.stop_time = 604800
+        # bldg = self.create_building(prj, 'SimpleBuilding ' + name, 2015, 1, 20)
+        # # Zone 1
+        # tz = self.create_thermal_zone(bldg, "Bed room", "tz_1", 20, 3)
+        # self.create_instance(OuterWall, tz, 5, 0, bldg.year_of_construction, "light")
+        # self.create_instance(OuterWall, tz, 4, 90, bldg.year_of_construction, "light")
+        # self.create_instance(OuterWall, tz, 5, 180, bldg.year_of_construction, "light")
+        # self.create_instance(OuterWall, tz, 4, 270, bldg.year_of_construction, "light")
+        # self.create_instance(Floor, tz, 20, -1, bldg.year_of_construction, "light")
+        # self.create_instance(Window, tz, 0.001, 0, bldg.year_of_construction, "EnEv")
+        # # Tabs Representation
+        # self.create_instance(Floor, tz, 20, -1, bldg.year_of_construction, "up_half_light")
+        # # self.create_instance(GroundFloor, tz, 20, -2, bldg.year_of_construction, "heavy")
+        # tz.calc_zone_parameters()
+        #
+        # # Zone 2
+        # tz2 = self.create_thermal_zone(bldg, "Bed room", "tz_2", 20, 3)
+        # self.create_instance(OuterWall, tz2, 5, 0, bldg.year_of_construction, "light")
+        # self.create_instance(OuterWall, tz2, 4, 90, bldg.year_of_construction, "light")
+        # self.create_instance(OuterWall, tz2, 5, 180, bldg.year_of_construction, "light")
+        # self.create_instance(OuterWall, tz2, 4, 270, bldg.year_of_construction, "light")
+        # self.create_instance(Floor, tz2, 20, -1, bldg.year_of_construction, "light")
+        # self.create_instance(Window, tz2, 0.001, 0, bldg.year_of_construction, "EnEv")
+        # # Tabs Representation
+        # # self.create_instance(Floor, tz2, 20, -1, bldg.year_of_construction, "heavy")
+        # self.create_instance(InnerTABS, tz2, 20, -1, bldg.year_of_construction, "up_half_light")
+        # # self.create_instance(OuterTABS, tz2, 20, -2, bldg.year_of_construction, "up_half_light")
+        # tz2.calc_zone_parameters()
+        #
+        # bldg.calc_building_parameter()
+        # prj.export_aixlib(path='D:/dja-dco/Dymola_Exports')
+
+        name = 'TypeM'
         prj = self.create_project(name)
         prj.modelica_info.stop_time = 604800
         bldg = self.create_building(prj, 'SimpleBuilding ' + name, 2015, 1, 20)
         # Zone 1
         tz = self.create_thermal_zone(bldg, "Bed room", "tz_1", 20, 3)
-        self.create_instance(OuterWall, tz, 5, 0, bldg.year_of_construction, "light")
-        self.create_instance(OuterWall, tz, 4, 90, bldg.year_of_construction, "light")
-        self.create_instance(OuterWall, tz, 5, 180, bldg.year_of_construction, "light")
-        self.create_instance(OuterWall, tz, 4, 270, bldg.year_of_construction, "light")
-        self.create_instance(Floor, tz, 20, -1, bldg.year_of_construction, "light")
-        self.create_instance(Window, tz, 0.001, 0, bldg.year_of_construction, "EnEv")
-        # Tabs Representation
-        self.create_instance(Floor, tz, 20, -1, bldg.year_of_construction, "up_half_light")
-        # self.create_instance(GroundFloor, tz, 20, -2, bldg.year_of_construction, "heavy")
+        self.create_instance(Floor, tz, 18.75, -2, bldg.year_of_construction, "light", RoomTypes[name]['Floor'])
+        self.create_instance(Rooftop, tz, 19.5, -1, bldg.year_of_construction, "light",  RoomTypes[name]['Rooftop'])
+        self.create_instance(InnerWall, tz, 36.51, 0, bldg.year_of_construction, "light",  RoomTypes[name]['InnerWall'])
+        self.create_instance(InnerWall, tz, 2, 0, bldg.year_of_construction, "light",  RoomTypes[name]['InnerDoor'])
+        self.create_instance(OuterWall, tz, 8.13, 180, bldg.year_of_construction, "light",  RoomTypes[name]['OuterWall'])
         tz.calc_zone_parameters()
-
-        # Zone 2
-        tz2 = self.create_thermal_zone(bldg, "Bed room", "tz_2", 20, 3)
-        self.create_instance(OuterWall, tz2, 5, 0, bldg.year_of_construction, "light")
-        self.create_instance(OuterWall, tz2, 4, 90, bldg.year_of_construction, "light")
-        self.create_instance(OuterWall, tz2, 5, 180, bldg.year_of_construction, "light")
-        self.create_instance(OuterWall, tz2, 4, 270, bldg.year_of_construction, "light")
-        self.create_instance(Floor, tz2, 20, -1, bldg.year_of_construction, "light")
-        self.create_instance(Window, tz2, 0.001, 0, bldg.year_of_construction, "EnEv")
-        # Tabs Representation
-        # self.create_instance(Floor, tz2, 20, -1, bldg.year_of_construction, "heavy")
-        self.create_instance(InnerTABS, tz2, 20, -1, bldg.year_of_construction, "up_half_light")
-        # self.create_instance(OuterTABS, tz2, 20, -2, bldg.year_of_construction, "up_half_light")
-        tz2.calc_zone_parameters()
 
         bldg.calc_building_parameter()
         prj.export_aixlib(path='D:/dja-dco/Dymola_Exports')
@@ -90,9 +106,19 @@ class ExportAll:
         tz.number_of_elements = 2
         return tz
 
-    def create_instance(self, class_instance, tz, area, orientation, year_of_construction, construction):
+    def create_instance(self, class_instance, tz, area, orientation, year_of_construction, construction,
+                        create_layers=None):
         inst = class_instance(parent=tz)
-        inst.load_type_element(year_of_construction, construction)
+        if not create_layers:
+            inst.load_type_element(year_of_construction, construction)
+        else:
+            for layer_number, items in create_layers.items():
+                layer = Layer(parent=inst)
+                layer.thickness = items['thickness']
+                material = Material(parent=layer)
+                material.density = items['density']
+                material.heat_capac = items['heat_capac']
+                material.thermal_conduc = items['thermal_conduc']
         inst.area = area
         inst.orientation = orientation
         self.tz_instances.append(inst)
@@ -242,6 +268,267 @@ class ExportAll:
                      )
         return r1, c1
 
+
+RoomTypes = {
+    'TypeM': {
+        'Floor': {
+            1: {
+                'thickness': 0.004,
+                'thermal_conduc': 0.17,
+                'density': 1200,
+                'heat_capac': 1.4
+            },
+            2: {
+                'thickness': 0.04,
+                'thermal_conduc': 1.4,
+                'density': 1000,
+                'heat_capac': 2
+            },
+            3: {
+                'thickness': 0.08,
+                'thermal_conduc': 0.434,
+                'density': 1.2,
+                'heat_capac': 1
+            },
+            4: {
+                'thickness': 0.2,
+                'thermal_conduc': 2.3,
+                'density': 2300,
+                'heat_capac': 1
+            },
+            5: {
+                'thickness': 0.255,
+                'thermal_conduc': 1.627,
+                'density': 1.2,
+                'heat_capac': 1
+            },
+            6: {
+                'thickness': 0.02,
+                'thermal_conduc': 0.04,
+                'density': 75,
+                'heat_capac': 1.03
+            },
+            7: {
+                'thickness': 0.001,
+                'thermal_conduc': 50,
+                'density': 7800,
+                'heat_capac': 0.45
+            }
+        },
+        'Rooftop': {
+            1: {
+                'thickness': 0.001,
+                'thermal_conduc': 50,
+                'density': 7800,
+                'heat_capac': 0.45
+            },
+            2: {
+                'thickness': 0.02,
+                'thermal_conduc': 0.04,
+                'density': 75,
+                'heat_capac': 1.03
+            },
+            3: {
+                'thickness': 0.255,
+                'thermal_conduc': 1.627,
+                'density': 1.2,
+                'heat_capac': 1
+            },
+            4: {
+                'thickness': 0.2,
+                'thermal_conduc': 2.3,
+                'density': 2300,
+                'heat_capac': 1
+            },
+            5: {
+                'thickness': 0.095,
+                'thermal_conduc': 0.04,
+                'density': 105,
+                'heat_capac': 1
+            },
+            6: {
+                'thickness': 0.005,
+                'thermal_conduc': 0.23,
+                'density': 1100,
+                'heat_capac': 1
+            },
+            7: {
+                'thickness': 0.2,
+                'thermal_conduc': 0.7,
+                'density': 1638,
+                'heat_capac': 1
+            }
+        },
+        'InnerWall': {
+            1: {
+                'thickness': 0.015,
+                'thermal_conduc': 0.57,
+                'density': 1300,
+                'heat_capac': 1
+            },
+            2: {
+                'thickness': 0.115,
+                'thermal_conduc': 0.58,
+                'density': 1400,
+                'heat_capac': 1
+            },
+            3: {
+                'thickness': 0.015,
+                'thermal_conduc': 0.57,
+                'density': 1300,
+                'heat_capac': 1
+            }
+        },
+        'InnerDoor': {
+            1: {
+                'thickness': 0.025,
+                'thermal_conduc': 0.2,
+                'density': 800,
+                'heat_capac': 1.6
+            }
+        },
+        'OuterWall': {
+            1: {
+                'thickness': 0.08,
+                'thermal_conduc': 1.65,
+                'density': 2200,
+                'heat_capac': 1
+            },
+            2: {
+                'thickness': 0.08,
+                'thermal_conduc': 0.035,
+                'density': 10,
+                'heat_capac': 1.03
+            },
+            3: {
+                'thickness': 0.03,
+                'thermal_conduc': 0.163,
+                'density': 1.2,
+                'heat_capac': 1
+            },
+            4: {
+                'thickness': 0.012,
+                'thermal_conduc': 0.6,
+                'density': 1650,
+                'heat_capac': 1
+            }
+        }
+    },
+    'TypeS': {
+        'Floor': {
+            1: {
+                'thickness': 0.004,
+                'thermal_conduc': 0.17,
+                'density': 1200,
+                'heat_capac': 1.4
+            },
+            2: {
+                'thickness': 0.045,
+                'thermal_conduc': 1.4,
+                'density': 2000,
+                'heat_capac': 1
+            },
+            3: {
+                'thickness': 0.03,
+                'thermal_conduc': 0.045,
+                'density': 135,
+                'heat_capac': 1.03
+            },
+            4: {
+                'thickness': 0.24,
+                'thermal_conduc': 2.3,
+                'density': 2300,
+                'heat_capac': 1
+            },
+            5: {
+                'thickness': 0.015,
+                'thermal_conduc': 0.570,
+                'density': 1300,
+                'heat_capac': 1
+            }
+        },
+        'Rooftop': {
+            1: {
+                'thickness': 0.015,
+                'thermal_conduc': 0.57,
+                'density': 1300,
+                'heat_capac': 1
+            },
+            2: {
+                'thickness': 0.24,
+                'thermal_conduc': 2.3,
+                'density': 2300,
+                'heat_capac': 1
+            },
+            3: {
+                'thickness': 0.12,
+                'thermal_conduc': 0.04,
+                'density': 105,
+                'heat_capac': 1
+            },
+            4: {
+                'thickness': 0.005,
+                'thermal_conduc': 0.23,
+                'density': 1100,
+                'heat_capac': 1
+            },
+            5: {
+                'thickness': 0.2,
+                'thermal_conduc': 0.700,
+                'density': 1638,
+                'heat_capac': 1
+            }
+        },
+        'InnerWall': {
+            1: {
+                'thickness': 0.015,
+                'thermal_conduc': 0.57,
+                'density': 1300,
+                'heat_capac': 1
+            },
+            2: {
+                'thickness': 0.175,
+                'thermal_conduc': 0.58,
+                'density': 1400,
+                'heat_capac': 1
+            },
+            3: {
+                'thickness': 0.015,
+                'thermal_conduc': 0.57,
+                'density': 1300,
+                'heat_capac': 1
+            }
+        },
+        'InnerDoor': {
+            1: {
+                'thickness': 0.025,
+                'thermal_conduc': 0.2,
+                'density': 800,
+                'heat_capac': 1.6
+            }
+        },
+        'OuterWall': {
+            1: {
+                'thickness': 0.12,
+                'thermal_conduc': 1.65,
+                'density': 2200,
+                'heat_capac': 1
+            },
+            2: {
+                'thickness': 0.085,
+                'thermal_conduc': 0.035,
+                'density': 10,
+                'heat_capac': 1.03
+            },
+            3: {
+                'thickness': 0.012,
+                'thermal_conduc': 0.6,
+                'density': 1650,
+                'heat_capac': 1
+            }
+        }
+    }
+}
 
 if __name__ == "__main__":
     export = ExportAll()
