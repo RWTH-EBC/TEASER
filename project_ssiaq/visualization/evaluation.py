0217096276b1c3838d125c31d469a83cad35e9bd
paul.seiwert <pseiwert@eonerc.rwth-aachen.de>
2023-04-28 16:38:41 +0200

    adapt colormap for plotting and add save option

--- project_ssiaq/visualization/evaluation.py
+++ project_ssiaq/visualization/evaluation.py
@@ -2,17 +2,19 @@ from pathlib import Path
 from ebcpy import TimeSeriesData
 import datetime
 import matplotlib.pyplot as plt
-
+import numpy as np
+import pathlib
+import os
 
 def import_and_process(path):
     tsd = TimeSeriesData(path)
     tsd.to_datetime_index(origin=datetime.datetime(2022, 1, 1, 0, 0, 0))
     return tsd
 
-def plot_T_Air(tsd, building_type,zone_map):
-    plt.figure()
-    color_map = ['blue', 'red', 'green', 'orange', 'pink', 'black', 'yellow', 'purple', 'brown', 'grey']
+def plot_T_Air(tsd, building_type, scenario, zone_map,color_map):
+    plt.figure(dpi=300)
 
+    #start_dt = datetime.datetime.strptime("01/02/22 00:00:00",'%m/%d/%y %H:%M:%S')
     for idx, zone in enumerate(zone_map[building_type]):
         plot_param = "multizone.TAir[" + str(idx + 1) + "]"
         label = "T_air - " + str(zone)
@@ -20,45 +22,47 @@ def plot_T_Air(tsd, building_type,zone_map):
 
     plt.xlabel('Time')
     plt.ylabel('Air temperature in °C')
-    plt.legend()
-    plt.show()
+    plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
+    #plt.show()
+    plt.savefig("T_Air"+scenario+".png",bbox_inches="tight")
+    plt.close()
+def plot_Q_flow(tsd, building_type, scenario, zone_map,color_map):
+    plt.figure(dpi=300)
 
-
-def plot_Q_flow(tsd, building_type,zone_map):
-    plt.figure()
-    color_map = ['blue', 'red', 'green', 'orange', 'pink', 'black', 'yellow', 'purple', 'brown', 'grey']
-
+    #start_dt = datetime.datetime.strptime("01/02/22 00:00:00",'%m/%d/%y %H:%M:%S')
     for idx, zone in enumerate(zone_map[building_type]):
         plot_param_1 = "multizone.PHeater[" + str(idx + 1) + "]"
         plot_param_2 = "multizone.PCooler[" + str(idx + 1) + "]"
         label_1 = "P_Heater - " + str(zone)
         label_2 = "P_Cooler - " + str(zone)
         plt.plot(tsd.loc[:, (plot_param_1, "raw")], label=label_1, color=color_map[idx])
-        plt.plot(tsd.loc[:, (plot_param_2, "raw")], label=label_2, color=color_map[idx])
+        #plt.plot(-tsd.loc[:, (plot_param_2, "raw")], label=label_2, color=color_map[idx])
 
     plt.xlabel('Time')
     plt.ylabel('Power in W')
-    plt.legend()
-    plt.show()
-
-
+    plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
+    #plt.show()
+    plt.savefig("Q_flow" + scenario + ".png",bbox_inches="tight")
+    plt.close()
 def get_bldg_type_from_filename(filename):
     building_types = ['single_family_house', 'multi_family_house', 'terraced_house', 'apartment_block', 'office',
                       'school', 'hotel']
     for i in building_types:
-        if i in path_in_str:
+        if i in filename:
             building_type = i
+            scenario = pathlib.Path(filename).parts[-2].split('_')[0]
             break
-    return building_type
+    return building_type, scenario
 
 
 if __name__ == '__main__':
-    setup_name = "hotel_test"
+    setup_name = "20230307_referenz_komplett"
     basepath = Path('N:/Forschung/EBC0741_ZIM_SmartSenseIAQ_NK/Data/Simulationen/01_Referenzszenarien').joinpath(
         setup_name, "sim_results")
     # find all result files in given setup folder
-    pathlist = Path(basepath).rglob('*.mat')
 
+    pathlist = Path(basepath).rglob('*.mat')
+    #pathlist = Path("N:\Forschung\EBC0741_ZIM_SmartSenseIAQ_NK\Data\Simulationen/01_Referenzszenarien/20230307_referenz_komplett\sim_results\S157_school").rglob('*.mat')
     # TODO: Configure zone map
     zone_map = dict(office=['Office', 'Floor', 'Storage', 'Meeting', 'Restroom', 'ICT'],
                     school=['Classrooms', 'Floor', 'Storage', 'Office', 'Restroom', 'Further common rooms','Canteen',
@@ -68,9 +72,14 @@ if __name__ == '__main__':
                     single_family_house=['Single zone'], multi_family_house=['Single zone'],
                     terraced_house=['Single zone'], apartment_block=['Single zone'])
 
+
+    cmap = plt.get_cmap('plasma')
+
     for path in pathlist:
         path_in_str = str(path)
-        building_type = get_bldg_type_from_filename(path_in_str)
+        building_type, scenario = get_bldg_type_from_filename(path_in_str)
         tsd = import_and_process(path_in_str)
-        plot_T_Air(tsd, building_type,zone_map)
-        plot_Q_flow(tsd, building_type,zone_map)
+        slicedCM = cmap(np.linspace(0, 1, len(zone_map[building_type])))
+        os.chdir(path.parents[0])
+        plot_T_Air(tsd, building_type, scenario, zone_map,slicedCM)
+        plot_Q_flow(tsd, building_type, scenario, zone_map,slicedCM)
