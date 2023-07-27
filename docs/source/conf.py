e2749cf58da4f3ac147d2b4429142dc1011ae0f2
fabian.wuellhorst <fabian.wuellhorst@rwth-aachen.de>
2023-07-27 16:54:22 +0200

    chore: update extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,15 @@ from pathlib import Path
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinxcontrib.bibtex", "sphinx.ext.autodoc", "numpydoc"]
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.ifconfig',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.githubpages',
+              'sphinx.ext.coverage',
+              'm2r2',  # Enable .md files
+              'sphinxcontrib.autodoc_pydantic'
+              ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
