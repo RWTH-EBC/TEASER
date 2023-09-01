f3a616ae47468b6a80e6519e6fb0bd0ba64fa95f
fabian.wuellhorst <fabian.wuellhorst@rwth-aachen.de>
2023-09-01 15:55:47 +0200

    chore: trigger pypi-release [PYPI-RELEASE]

--- teaser/examples/e1_generate_archetype.py
+++ teaser/examples/e1_generate_archetype.py
@@ -1,6 +1,6 @@
 # # Example 1: Generate archetype buildings using TEASER API
-# This module contains an example how to generate archetype buildings using
-# TEASER API functions.
+# This module contains an example how to generate archetype buildings
+# using TEASER API functions.
 # You can run this example using the [jupyter-notebook](https://mybinder.org/v2/gh/RWTH-EBC/TEASER/master?labpath=docs%2Fjupyter_notebooks)
 
 
