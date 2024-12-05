f4105d8fe4ff3e99d2b134905951b03812ba3c9d
Hendrik van der Stok <hendrik.stok@rwth-aachen.de>
2024-12-05 16:01:26 +0100

    increas AixLib and BESMod version

--- teaser/logic/buildingobjects/calculation/aixlib.py
+++ teaser/logic/buildingobjects/calculation/aixlib.py
@@ -64,8 +64,8 @@ class AixLib(object):
         self.file_set_t_cool = "TsetCool_" + self.parent.name + ".txt"
         self.file_ahu = "AHU_" + self.parent.name + ".txt"
         self.file_internal_gains = "InternalGains_" + self.parent.name + ".txt"
-        self.version = "2.1.0"
-        self.besmod_version = "0.5.0"
+        self.version = "2.2.0"
+        self.besmod_version = "0.6.0"
         self.total_surface_area = None
         self.consider_heat_capacity = True
         self.use_set_back = True
